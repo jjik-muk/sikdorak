28e6ca925dcdb6fa3f774bec67297b56d7fc0d5e
youryu0212 <youryu0212@naver.com>
2022-07-25 15:55:19 +0900

    feat: :sparkles: 로그인 인풋 컴포넌트 구현
    Co-authored-by: rumka <yongseongjeon@users.noreply.github.com>

---
+++ fe/src/components/Login/LoginInput/LoginInput.styled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrap = styled.input`
+  width: 340px;
+  height: 64px;
+  background-color: #eff0f6;
+  font-size: 16px;
+  border-radius: 16px;
+  padding: 16px 24px;
+
+  // TODO: reset CSS 적용한 후 삭제
+  border: 0;
+  box-sizing: border-box;
+`;
