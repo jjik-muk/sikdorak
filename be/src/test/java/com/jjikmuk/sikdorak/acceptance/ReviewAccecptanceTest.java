d8241af50717c638fc568415ce01f1a1af228756
jinan159 <wlsdhks0423@naver.com>
2022-07-25 12:53:49 +0900

    test: :white_check_mark: 리뷰 작성 시 평점이 유효하지 않은 경우 테스트 코드 통과
    Co-authored-by: ku-kim <kukim.dev@gmail.com>
    Co-authored-by: jinan159 <wlsdhks0423@naver.com>
    Co-authored-by: seokho-ham <kalejuice02.gmail.com>

--- be/src/test/java/com/jjikmuk/sikdorak/acceptance/ReviewAccecptanceTest.java
+++ be/src/test/java/com/jjikmuk/sikdorak/acceptance/ReviewAccecptanceTest.java
@@ -1,12 +1,5 @@
 package com.jjikmuk.sikdorak.acceptance;
 
-import static io.restassured.RestAssured.given;
-
-import com.jjikmuk.sikdorak.store.domain.Store;
-import com.jjikmuk.sikdorak.store.repository.StoreRepository;
-
-import java.time.LocalDate;
-import java.util.stream.Stream;
 import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,16 +7,20 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.given;
 
 /**
  *  [x] 요청 텍스트가 유효하지 않은 경우(null, empty, 500자 넘는 경우)
  *  [x] 요청 식당 id가 유효하지 않은 경우(null, 0, -1, 등록되지 않은 값)
  *  [x] 요청 방문일이 유효하지 않은 경우(미래 날짜, 유효하지 않은 날짜 형식)
- *  [ ] 요청 평점이 유효하지 않은 경우(1,2,3,4,5 가 아닌 경우)
+ *  [x] 요청 평점이 유효하지 않은 경우(1,2,3,4,5 가 아닌 경우)
  *  [ ] 요청 태그들이 유효하지 않은 경우(공백 포함, 한글 영어 숫자 이외의 값, 50자 초과, 개수 30개 초과)
  *  [ ] 요청 공개 범위가 유효하지 않은 경우(public, protected, private 이외의 값, null, empty)
  */
@@ -111,7 +108,6 @@ public class ReviewAccecptanceTest extends InitAcceptanceTest {
 			"02-01, 400"
 	})
 	@DisplayName("리뷰 생성 요청에서 방문일이 유효하지 않은 경우 에러 상태코드를 반환한다")
-	// (미래 날짜, 유효하지 않은 날짜 형식)
 	void create_review_visitedDate_invalid_failed(String localDate, int expectedStatusCode) {
 		JSONObject requestBody = new JSONObject();
 		requestBody.put("reviewContent", "찐맛집입니다.");
@@ -135,6 +131,32 @@ public class ReviewAccecptanceTest extends InitAcceptanceTest {
 				.then()
 				.statusCode(expectedStatusCode);
 
+	}
+
+	@ParameterizedTest
+	@NullSource
+	@ValueSource(floats = {0f, -1f, 1.1f, 6f})
+	@DisplayName("리뷰 생성 요청에서 평점이 유효하지 않은 경우 에러 상태코드를 반환한다")
+	void create_review_reviewScore_invalid_failed(Float reviewScore) {
+		JSONObject requestBody = new JSONObject();
+		requestBody.put("reviewContent", "찐맛집입니다.");
+		requestBody.put("storeId", savedStore.getId());
+		requestBody.put("reviewScore", reviewScore);
+		requestBody.put("reviewVisibility", "public");
+		requestBody.put("visitedDate", "2022-01-01");
+		requestBody.put("tags", new String[]{"맛집", "꿀맛"});
+		requestBody.put("images", new String[]{"https://s3.ap-northeast-2.amazonaws.com/sikdorak/test.jpg"});
+
+		given()
+				.accept(MediaType.APPLICATION_JSON_VALUE)
+				.header("Content-type", "application/json")
+				.body(requestBody)
+
+				.when()
+				.post("/api/reviews")
+
+				.then()
+				.statusCode(HttpStatus.BAD_REQUEST.value());
 	}
 
 	private static Stream<Arguments> provideContentForIsNullAndEmptyAnd500char() {
