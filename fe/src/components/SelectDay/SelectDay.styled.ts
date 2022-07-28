8ba5a6a142b0476514ec916c5c5a88cfbf50a72c
youryu0212 <youryu0212@naver.com>
2022-07-28 11:56:41 +0900

    feat: :sparkles: 날짜 선택 컴포넌트 구현

---
+++ fe/src/components/SelectDay/SelectDay.styled.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SelectDayWrapper = styled.button`
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+`;
---
+++ fe/src/components/SelectDay/SelectDay.tsx
@@ -0,0 +1,5 @@
+import { SelectDayWrapper } from './SelectDay.styled';
+
+export default function SelectDay() {
+  return <SelectDayWrapper>방문일 선택</SelectDayWrapper>;
+}
