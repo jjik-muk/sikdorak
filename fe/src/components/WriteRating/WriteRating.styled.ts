dd646edf2c1bb994f267ec2b88ace85e20ad5b7c
yongseongjeon <yongseong.jeon95@gmail.com>
2022-07-28 11:37:52 +0900

    feat: :sparkles: WriteRating 컴포넌트 추가

---
+++ fe/src/components/WriteRating/WriteRating.styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+  display: flex;
+  gap: 175px;
+`;
+
+const Text = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export { Wrap, Text };
---
+++ fe/src/components/WriteRating/WriteRating.tsx
@@ -0,0 +1,13 @@
+import StarRating from 'components/StarRating/StarRating';
+import { Text, Wrap } from './WriteRating.styled';
+
+function WriteRating() {
+  return (
+    <Wrap>
+      <Text>평점 입력</Text>
+      <StarRating />
+    </Wrap>
+  );
+}
+
+export default WriteRating;
