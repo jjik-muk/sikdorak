663dd56c03969bd242cc67bad949593b77acf5fd
youryu0212 <youryu0212@naver.com>
2022-07-28 11:54:56 +0900

    feat: :sparkles: 이미지 업로드 버튼 컴포넌트 구현

---
+++ fe/src/components/ImgUpload/Button/Button.styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const ButtonWrapper = styled.div`
+  background-color: #7d91d9;
+  color: #fff;
+  font-size: 14px;
+  padding: 10px;
+  border-radius: 10px;
+`;
---
+++ fe/src/components/ImgUpload/Button/Button.tsx
@@ -0,0 +1,7 @@
+import { ButtonWrapper } from './Button.styled';
+
+function Button() {
+  return <ButtonWrapper>사진 업로드</ButtonWrapper>;
+}
+
+export default Button;
