ce6ec2de604c2b22916940382125de403aabff72
“kukim” <kukim.dev@gmail.com>
2022-07-29 15:42:03 +0900

    infra: :bricks: setup heroku provider in Terraform

---
+++ infra/be/variables.tf
@@ -0,0 +1,11 @@
+variable "heroku_email" {
+  description = "Heroku email"
+}
+
+variable "heroku_api_key" {
+  description = "Heroku private api key"
+}
+
+variable "heroku_app_name" {
+  description = "Heroku app name"
+}
