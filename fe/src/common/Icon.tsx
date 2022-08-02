0b45a96d5a243993ceca5d2650ccfb6553480eea
youryu0212 <youryu0212@naver.com>
2022-08-02 23:55:20 +0900

    feat: :sparkles: 좋아요, 댓글, 공유하기 아이콘 추가

--- fe/src/common/Icon.tsx
+++ fe/src/common/Icon.tsx
@@ -1,7 +1,11 @@
 import SIZE from 'constants/size';
 import { ReactComponent as DownArrow } from 'assets/DownArrow.svg';
+import { ReactComponent as Heart } from 'assets/Heart.svg';
+import { ReactComponent as MenuBtn } from 'assets/MenuBtn.svg';
 import { ReactComponent as Photo } from 'assets/Photo.svg';
+import { ReactComponent as ShareArrow } from 'assets/ShareArrow.svg';
 import { ReactComponent as Star } from 'assets/Star.svg';
+import { ReactComponent as TalkBubble } from 'assets/TalkBubble.svg';
 import { ReactComponent as UpArrow } from 'assets/UpArrow.svg';
 
 const iconComponents = {
@@ -9,6 +13,10 @@ const iconComponents = {
   DownArrow,
   UpArrow,
   Star,
+  MenuBtn,
+  Heart,
+  ShareArrow,
+  TalkBubble,
 };
 
 function Icon({
