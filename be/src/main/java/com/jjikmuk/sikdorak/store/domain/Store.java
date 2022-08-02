cd4d07a1a449864a9c4924d868362a30a106f8d3
jinan159 <wlsdhks0423@naver.com>
2022-08-02 14:21:57 +0900

    feat: :sparkles: Store 도메인 정의
    - StoreName 추가
    - ContactNumber 추가
    - Address 추가
    - StoreLocation 추가

--- be/src/main/java/com/jjikmuk/sikdorak/store/domain/Store.java
+++ be/src/main/java/com/jjikmuk/sikdorak/store/domain/Store.java
@@ -4,6 +4,7 @@ import com.jjikmuk.sikdorak.common.domain.BaseTimeEntity;
 import lombok.Getter;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,6 +17,19 @@ public class Store extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "storeId")
+    @Column(name = "store_id")
     private Long id;
+
+    @Embedded
+    private StoreName storeName;
+
+    @Embedded
+    private ContactNumber contactNumber;
+
+    @Embedded
+    private Address address; // baseAddress, detailAddress
+
+    @Embedded
+    private StoreLocation location; // Float latitude, Float longitude
+
 }
