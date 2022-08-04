662ec3c2d36e2d718b9ff37dfaee4ceb9cec1d60
“kukim” <kukim.dev@gmail.com>
2022-08-04 14:49:16 +0900

    refactor :recycle: 불필요한 인수테스트 제거와 REST Docs 문서 포맷

--- be/src/test/java/com/jjikmuk/sikdorak/acceptance/InitAcceptanceTest.java
+++ be/src/test/java/com/jjikmuk/sikdorak/acceptance/InitAcceptanceTest.java
@@ -2,14 +2,20 @@ package com.jjikmuk.sikdorak.acceptance;
 
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyUris;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jjikmuk.sikdorak.common.DatabaseConfigurator;
 import com.jjikmuk.sikdorak.common.MysqlTestContainer;
 import com.jjikmuk.sikdorak.store.domain.Store;
 import com.jjikmuk.sikdorak.store.repository.StoreRepository;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.ObjectMapperConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.internal.mapping.Jackson2Mapper;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +27,8 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.operation.preprocess.OperationPreprocessor;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ExtendWith(RestDocumentationExtension.class)
@@ -30,6 +38,16 @@ public class InitAcceptanceTest extends MysqlTestContainer {
 	protected static final String DEFAULT_RESTDOC_SCHEMA = "https";
 	protected static final String DEFAULT_RESTDOC_HOST = "sikdorak.herokuapp.com";
 
+	public static final FieldDescriptor DEFAULT_CODE_RESPONSE_FIELD_DESCRIPTOR =
+		fieldWithPath("code")
+		.type(JsonFieldType.STRING)
+		.description("식도락 서버 전용 상태코드");
+
+	public static final FieldDescriptor DEFAULT_MESSAGE_RESPONSE_FIELD_DESCRIPTORS =
+		fieldWithPath("message")
+		.type(JsonFieldType.STRING)
+		.description("자세한 응답 메세지");
+
 	protected RequestSpecification spec;
 
 	@Autowired
@@ -38,10 +56,17 @@ public class InitAcceptanceTest extends MysqlTestContainer {
 	@Autowired
 	StoreRepository storeRepository;
 
+	@Autowired
+	protected ObjectMapper objectMapper;
+
 	@LocalServerPort
 	int port;
 
-	Store savedStore;
+	public Store savedStore;
+
+	{
+		setUpRestAssured();
+	}
 
 	@BeforeEach
 	void setUp() {
@@ -51,17 +76,18 @@ public class InitAcceptanceTest extends MysqlTestContainer {
 
 	@BeforeEach
 	void setUpRestDocs(RestDocumentationContextProvider restDocumentation) {
-		OperationPreprocessor requestOperationProcessor = modifyUris().scheme(DEFAULT_RESTDOC_SCHEMA)
-				.host(DEFAULT_RESTDOC_HOST)
-				.removePort();
+		OperationPreprocessor requestOperationProcessor = modifyUris().scheme(
+				DEFAULT_RESTDOC_SCHEMA)
+			.host(DEFAULT_RESTDOC_HOST)
+			.removePort();
 
 		this.spec = new RequestSpecBuilder()
-				.setPort(port)
-				.addFilter(documentationConfiguration(restDocumentation)
-						.operationPreprocessors()
-						.withRequestDefaults(requestOperationProcessor, prettyPrint())
-						.withResponseDefaults(prettyPrint()))
-				.build();
+			.setPort(port)
+			.addFilter(documentationConfiguration(restDocumentation)
+				.operationPreprocessors()
+				.withRequestDefaults(requestOperationProcessor, prettyPrint())
+				.withResponseDefaults(prettyPrint()))
+			.build();
 	}
 
 	@AfterEach
@@ -69,4 +95,10 @@ public class InitAcceptanceTest extends MysqlTestContainer {
 		databaseConfigurator.clear();
 	}
 
+	private void setUpRestAssured() {
+		RestAssured.config = RestAssuredConfig.config()
+			.objectMapperConfig(
+				new ObjectMapperConfig(
+					new Jackson2Mapper((type, charset) -> objectMapper)));
+	}
 }
---
+++ be/src/test/java/com/jjikmuk/sikdorak/acceptance/review/ReviewSnippet.java
@@ -0,0 +1,48 @@
+package com.jjikmuk.sikdorak.acceptance.review;
+
+import static com.jjikmuk.sikdorak.acceptance.DocumentFormatGenerator.fieldWithPathAndValidConstraints;
+import static com.jjikmuk.sikdorak.acceptance.InitAcceptanceTest.DEFAULT_CODE_RESPONSE_FIELD_DESCRIPTOR;
+import static com.jjikmuk.sikdorak.acceptance.InitAcceptanceTest.DEFAULT_MESSAGE_RESPONSE_FIELD_DESCRIPTORS;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+
+import com.jjikmuk.sikdorak.review.controller.request.ReviewInsertRequest;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.snippet.Snippet;
+
+interface ReviewSnippet {
+
+	Snippet REVIEW_INSERT_REQUEST_SNIPPET = requestFields(
+		fieldWithPathAndValidConstraints("reviewContent", ReviewInsertRequest.class)
+			.type(JsonFieldType.STRING)
+			.description("리뷰 내용"),
+		fieldWithPathAndValidConstraints("storeId", ReviewInsertRequest.class)
+			.type(JsonFieldType.NUMBER)
+			.description("가게 아이디"),
+		fieldWithPathAndValidConstraints("reviewScore", ReviewInsertRequest.class)
+			.type(JsonFieldType.NUMBER)
+			.description("리뷰 점수"),
+		fieldWithPathAndValidConstraints("reviewVisibility", ReviewInsertRequest.class)
+			.type(JsonFieldType.STRING)
+			.description("리뷰 게시물의 공개 범위"),
+		fieldWithPathAndValidConstraints("visitedDate", ReviewInsertRequest.class)
+			.type(JsonFieldType.STRING)
+			.description("가게 방문일"),
+		fieldWithPathAndValidConstraints("tags", ReviewInsertRequest.class)
+			.type(JsonFieldType.ARRAY)
+			.description("리뷰를 표현하는 태그들"),
+		fieldWithPathAndValidConstraints("images", ReviewInsertRequest.class)
+			.type(JsonFieldType.ARRAY)
+			.description("리뷰를 위한 사진 URL")
+	);
+
+	Snippet REVIEW_INSERT_RESPONSE_SNIPPET = responseFields(
+		DEFAULT_CODE_RESPONSE_FIELD_DESCRIPTOR,
+		DEFAULT_MESSAGE_RESPONSE_FIELD_DESCRIPTORS,
+		fieldWithPath("data")
+			.type(JsonFieldType.NULL)
+			.description("리뷰 생성 데이터는 없습니다.")
+	);
+
+}
