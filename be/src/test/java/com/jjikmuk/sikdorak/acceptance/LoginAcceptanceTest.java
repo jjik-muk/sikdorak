a73783d48343024e1211f7befc1e63e8ffcb3fce
함석호 <kalejuice02@gmail.com>
2022-08-04 16:02:50 +0900

    test: :white_check_mark: WireMock을 사용한 로그인 인수테스트 작성 및 통과

--- be/src/test/java/com/jjikmuk/sikdorak/acceptance/LoginAcceptanceTest.java
+++ be/src/test/java/com/jjikmuk/sikdorak/acceptance/LoginAcceptanceTest.java
@@ -1,14 +1,34 @@
 package com.jjikmuk.sikdorak.acceptance;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.jjikmuk.sikdorak.integration.auth.OAuthMocks;
+import com.jjikmuk.sikdorak.integration.auth.WireMockConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.startsWith;
 
+@ContextConfiguration(classes = { WireMockConfig.class })
 public class LoginAcceptanceTest extends InitAcceptanceTest {
+
+    @Autowired
+    WireMockServer wireMockServer;
+
+    @BeforeEach
+    void setResponse() throws IOException, URISyntaxException {
+        OAuthMocks.setupMockTokenResponse(wireMockServer);
+        OAuthMocks.setupMockUserInfoResponse(wireMockServer);
+    }
 
     @Test
     @DisplayName("유저가 로그인 버튼을 눌렀을 경우 카카오 로그인 페이지 url을 반환해야 한다.")
@@ -21,20 +41,18 @@ public class LoginAcceptanceTest extends InitAcceptanceTest {
                 .statusCode(HttpStatus.MOVED_PERMANENTLY.value())
                 .header("Location", startsWith("https://kauth.kakao.com/oauth/authorize"));
     }
-//
-//    @Test
-//    @DisplayName("유저로부터 로그인 요청이 들어오면 유저의 정보를 저장하고 토큰을 발급한다.")
-//    void new_user_login_success() {
-//        given()
-//                .queryParam("code", "code")
-//                .accept(MediaType.APPLICATION_JSON_VALUE)
-//                .when()
-//                .get("/api/oauth/callback")
-//                .then()
-//                .statusCode(HttpStatus.OK.value())
-//                .body("accessToken", notNullValue())
-//                .body("accessToken", equalTo("testAccessToken"))
-//                .body("refreshToken", notNullValue())
-//                .body("accessToken", equalTo("testRefreshToken"));
-//    }
+
+    @Test
+    @DisplayName("유저로부터 로그인 요청이 들어오면 유저의 정보를 저장하고 토큰을 발급한다.")
+    void new_user_login_success() {
+        given()
+                .queryParam("code", "code")
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/api/oauth/callback")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("data.accessToken", notNullValue())
+                .body("data.refreshToken", notNullValue());
+    }
 }
--- be/src/test/java/com/jjikmuk/sikdorak/common/mock/OAuthMocks.java
+++ be/src/test/java/com/jjikmuk/sikdorak/common/mock/OAuthMocks.java
@@ -1,19 +1,18 @@
 package com.jjikmuk.sikdorak.integration.auth;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static java.nio.charset.Charset.defaultCharset;
 import static org.springframework.util.StreamUtils.copyToString;
 
 public class OAuthMocks {
-    public static void setupMockTokenResponse(WireMockServer wireMockServer) throws IOException {
-        wireMockServer.stubFor(WireMock.post(WireMock.urlEqualTo("/oauth/token?grant_type=authorization_code&client_id=1234&redirect_uri=redirectUri&code=code"))
+    public static void setupMockTokenResponse() throws IOException {
+        stubFor(WireMock.post(WireMock.urlEqualTo("/oauth/token?grant_type=authorization_code&client_id=1234&redirect_uri=redirectUri&code=code"))
                 .willReturn(WireMock.aResponse()
                         .withStatus(HttpStatus.OK.value())
                         .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
@@ -23,8 +22,8 @@ public class OAuthMocks {
                                         defaultCharset()))));
     }
 
-    public static void setupMockUserInfoResponse(WireMockServer wireMockServer) throws IOException, URISyntaxException {
-        wireMockServer.stubFor(WireMock.get(WireMock.urlEqualTo("/v2/user/me"))
+    public static void setupMockUserInfoResponse() throws IOException{
+        stubFor(WireMock.get(WireMock.urlEqualTo("/v2/user/me"))
                 .willReturn(WireMock.aResponse()
                         .withStatus(HttpStatus.OK.value())
                         .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
