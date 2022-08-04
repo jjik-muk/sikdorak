58b6b6f21b30ce4ef075a9f3cfb9d801756d918f
“kukim” <kukim.dev@gmail.com>
2022-08-04 17:41:43 +0900

    refactor: :recycle: Snippet 중복 코드 리팩토링
    Co-authored-by: jinan159 <wlsdhks0423@naver.com>

--- be/src/test/java/com/jjikmuk/sikdorak/acceptance/review/ReviewSnippet.java
+++ be/src/test/java/com/jjikmuk/sikdorak/acceptance/review/ReviewSnippet.java
@@ -1,8 +1,7 @@
 package com.jjikmuk.sikdorak.acceptance.review;
 
-import static com.jjikmuk.sikdorak.acceptance.DocumentFormatGenerator.DEFAULT_CODE_RESPONSE_FIELD_DESCRIPTOR;
-import static com.jjikmuk.sikdorak.acceptance.DocumentFormatGenerator.DEFAULT_MESSAGE_RESPONSE_FIELD_DESCRIPTORS;
-import static com.jjikmuk.sikdorak.acceptance.DocumentFormatGenerator.fieldWithPathAndValidConstraints;
+import static com.jjikmuk.sikdorak.acceptance.DocumentFormatGenerator.commonRequestFieldsWithValidConstraints;
+import static com.jjikmuk.sikdorak.acceptance.DocumentFormatGenerator.commonResponseNonFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
@@ -13,36 +12,30 @@ import org.springframework.restdocs.snippet.Snippet;
 
 interface ReviewSnippet {
 
-	Snippet REVIEW_INSERT_REQUEST_SNIPPET = requestFields(
-		fieldWithPathAndValidConstraints("reviewContent", ReviewInsertRequest.class)
+	Snippet REVIEW_INSERT_REQUEST_SNIPPET = commonRequestFieldsWithValidConstraints(
+		ReviewInsertRequest.class,
+		fieldWithPath("reviewContent")
 			.type(JsonFieldType.STRING)
 			.description("리뷰 내용"),
-		fieldWithPathAndValidConstraints("storeId", ReviewInsertRequest.class)
+		fieldWithPath("storeId")
 			.type(JsonFieldType.NUMBER)
 			.description("가게 아이디"),
-		fieldWithPathAndValidConstraints("reviewScore", ReviewInsertRequest.class)
+		fieldWithPath("reviewScore")
 			.type(JsonFieldType.NUMBER)
 			.description("리뷰 점수"),
-		fieldWithPathAndValidConstraints("reviewVisibility", ReviewInsertRequest.class)
+		fieldWithPath("reviewVisibility")
 			.type(JsonFieldType.STRING)
 			.description("리뷰 게시물의 공개 범위"),
-		fieldWithPathAndValidConstraints("visitedDate", ReviewInsertRequest.class)
+		fieldWithPath("visitedDate")
 			.type(JsonFieldType.STRING)
 			.description("가게 방문일"),
-		fieldWithPathAndValidConstraints("tags", ReviewInsertRequest.class)
+		fieldWithPath("tags")
 			.type(JsonFieldType.ARRAY)
 			.description("리뷰를 표현하는 태그들"),
-		fieldWithPathAndValidConstraints("images", ReviewInsertRequest.class)
+		fieldWithPath("images")
 			.type(JsonFieldType.ARRAY)
 			.description("리뷰를 위한 사진 URL")
 	);
 
-	Snippet REVIEW_INSERT_RESPONSE_SNIPPET = responseFields(
-		DEFAULT_CODE_RESPONSE_FIELD_DESCRIPTOR,
-		DEFAULT_MESSAGE_RESPONSE_FIELD_DESCRIPTORS,
-		fieldWithPath("data")
-			.type(JsonFieldType.NULL)
-			.description("리뷰 생성 데이터는 없습니다.")
-	);
-
+	Snippet REVIEW_INSERT_RESPONSE_SNIPPET = commonResponseNonFields();
 }
