0497d991f67688e22c18d0426633eaffd6feacad
함석호 <kalejuice02@gmail.com>
2022-08-04 11:36:45 +0900

    refactor: :recycle: Property 클래스에 Validate 추가

--- be/src/main/java/com/jjikmuk/sikdorak/common/properties/JwtProperties.java
+++ be/src/main/java/com/jjikmuk/sikdorak/common/properties/JwtProperties.java
@@ -4,17 +4,23 @@ package com.jjikmuk.sikdorak.common.properties;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.NotEmpty;
 import java.nio.charset.StandardCharsets;
 
 @Getter
+@Validated
 @ConstructorBinding
 @ConfigurationProperties(prefix = "secret")
 public class JwtProperties {
 
-    private byte[] secretKey;
-    private long accessTokenExpiredMillisecond;
-    private long refreshTokenExpiredMillisecond;
+    @NotEmpty
+    private final byte[] secretKey;
+    @NotEmpty
+    private final long accessTokenExpiredMillisecond;
+    @NotEmpty
+    private final long refreshTokenExpiredMillisecond;
 
     public JwtProperties(String secretKey, long accessTokenExpiredMillisecond, long refreshTokenExpiredMillisecond) {
         this.secretKey = secretKey.getBytes(StandardCharsets.UTF_8);
--- be/src/main/java/com/jjikmuk/sikdorak/common/properties/KakaoProperties.java
+++ be/src/main/java/com/jjikmuk/sikdorak/common/properties/KakaoProperties.java
@@ -3,16 +3,25 @@ package com.jjikmuk.sikdorak.common.properties;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
 
 @Getter
+@Validated
 @ConstructorBinding
 @ConfigurationProperties(prefix = "oauth.kakao")
 public class KakaoProperties {
 
+    @NotEmpty
     private final String loginPageUrl;
+    @NotEmpty
     private final String grantType;
+    @NotEmpty
     private final String clientId;
+    @NotEmpty
     private final String redirectUri;
+    @NotEmpty
     private final String responseType;
 
     public KakaoProperties(String loginPageUrl, String grantType, String clientId, String redirectUri, String responseType) {
