3f3ea6bdab4da8d83c5dcfed2c6040e3cb5e59c0
함석호 <kalejuice02@gmail.com>
2022-08-05 17:38:54 +0900

    refactor: :recycle: 객체와 리스트 snippet 생성 메서드 분리

--- be/src/test/java/com/jjikmuk/sikdorak/acceptance/auth/OAuthSnippet.java
+++ be/src/test/java/com/jjikmuk/sikdorak/acceptance/auth/OAuthSnippet.java
@@ -4,7 +4,7 @@ import com.jjikmuk.sikdorak.auth.controller.response.JwtTokenResponse;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.restdocs.snippet.Snippet;
 
-import static com.jjikmuk.sikdorak.acceptance.DocumentFormatGenerator.commonResponseFieldsWithValidConstraints;
+import static com.jjikmuk.sikdorak.acceptance.DocumentFormatGenerator.commonSingleResponseFieldsWithValidConstraints;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
@@ -15,13 +15,10 @@ public interface OAuthSnippet {
             parameterWithName("code").description("Kakao Authorization Code")
     );
 
-    Snippet LOGIN_SUCCESS_RESPONSE_SNIPPET = commonResponseFieldsWithValidConstraints(
+    Snippet LOGIN_SUCCESS_RESPONSE_SNIPPET = commonSingleResponseFieldsWithValidConstraints(
             JwtTokenResponse.class,
-            fieldWithPath("accessToken")
-                    .type(JsonFieldType.STRING)
-                    .description("액세스 토큰"),
-            fieldWithPath("refreshToken")
-                    .type(JsonFieldType.STRING)
-                    .description("리프레쉬 토큰")
+            fieldWithPath("accessToken").type(JsonFieldType.STRING).description("액세스 토큰"),
+            fieldWithPath("refreshToken").type(JsonFieldType.STRING).description("리프레쉬 토큰")
     );
+
 }
