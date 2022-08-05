f1baed2baaaea32b625c9ddb43b30fcb0310d6ba
jinan159 <wlsdhks0423@naver.com>
2022-08-05 17:48:23 +0900

    perf: :zap: 테스트 성능 향상을 위해 테스트 컨테이너 실행로직 개선

--- be/src/test/java/com/jjikmuk/sikdorak/common/MysqlTestContainer.java
+++ be/src/test/java/com/jjikmuk/sikdorak/common/MysqlTestContainer.java
@@ -1,7 +1,7 @@
 package com.jjikmuk.sikdorak.common;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
@@ -10,8 +10,11 @@ public abstract class MysqlTestContainer {
 	private static final String MYSQL_VERSION = "mysql:8";
 	private static final String DATABASE_NAME = "testDB";
 
-	@Container
 	static final MySQLContainer MYSQL_CONTAINER = new MySQLContainer(MYSQL_VERSION)
 		.withDatabaseName(DATABASE_NAME);
 
+	@BeforeAll
+	static void beforeAll() {
+		MYSQL_CONTAINER.start();
+	}
 }
