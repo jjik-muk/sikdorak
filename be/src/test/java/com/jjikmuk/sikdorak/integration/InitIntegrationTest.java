0e36483ddaf4d636b477d5c933ae1dafec939d3d
jinan159 <wlsdhks0423@naver.com>
2022-08-07 14:25:15 +0900

    perf: :zap: MySQL 컨테이너를 1회 실행하도록 변경하여 테스트 성능 향상

--- be/src/test/java/com/jjikmuk/sikdorak/integration/InitIntegrationTest.java
+++ be/src/test/java/com/jjikmuk/sikdorak/integration/InitIntegrationTest.java
@@ -1,14 +1,13 @@
 package com.jjikmuk.sikdorak.integration;
 
 import com.jjikmuk.sikdorak.common.DatabaseConfigurator;
-import com.jjikmuk.sikdorak.common.MysqlTestContainer;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
-public abstract class InitIntegrationTest extends MysqlTestContainer {
+public abstract class InitIntegrationTest {
 
 	@Autowired
 	protected DatabaseConfigurator testData;
