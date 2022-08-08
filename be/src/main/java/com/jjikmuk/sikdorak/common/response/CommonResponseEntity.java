c286ef38dd9bd1deaede877594fd9a89a663b6b9
SeokHo-Ham <kalejuice02@gmail.com>
2022-08-08 18:24:05 +0900

    refactor: :recycle: CommonResponseEntity에 header를 세팅하는 생성자 추가

--- be/src/main/java/com/jjikmuk/sikdorak/common/response/CommonResponseEntity.java
+++ be/src/main/java/com/jjikmuk/sikdorak/common/response/CommonResponseEntity.java
@@ -4,9 +4,17 @@ import com.jjikmuk.sikdorak.common.BaseResponse;
 import com.jjikmuk.sikdorak.common.CodeAndMessages;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 
 public class CommonResponseEntity<T> extends ResponseEntity<BaseResponse<T>> {
     public CommonResponseEntity(CodeAndMessages codeAndMessages, T data, HttpStatus httpStatus) {
-        super(new BaseResponse<>(codeAndMessages.getCode(), codeAndMessages.getMessage(), data), httpStatus);
+        this(codeAndMessages, data, null, httpStatus);
+
     }
+
+    public CommonResponseEntity(CodeAndMessages codeAndMessages, T data, MultiValueMap<String, String> headers,HttpStatus httpStatus) {
+        super(new BaseResponse<>(codeAndMessages.getCode(), codeAndMessages.getMessage(), data),
+            headers, httpStatus);
+    }
+
 }
