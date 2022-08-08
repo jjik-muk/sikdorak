3a611db88fd4410718c795233cb8138a267e19b3
SeokHo-Ham <kalejuice02@gmail.com>
2022-08-08 15:01:51 +0900

    refactor: :recycle: OAuthMock 클래스에 setup 메서드 작성

--- be/src/main/java/com/jjikmuk/sikdorak/user/domain/ProfileImage.java
+++ be/src/main/java/com/jjikmuk/sikdorak/user/domain/ProfileImage.java
@@ -1,20 +1,19 @@
 package com.jjikmuk.sikdorak.user.domain;
 
 import com.jjikmuk.sikdorak.auth.exception.InvalidUserProfileImageUrlException;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import javax.persistence.Embeddable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Embeddable;
-import java.util.Objects;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Getter
 @NoArgsConstructor
 @Embeddable
 public class ProfileImage {
 
-    private static final String URL_REGEX = "^((((https?|http?)://)|(mailto:|news:))(%[0-9A-Fa-f]{2}|[-()_.!~*';/?:@&=+$,A-Za-z0-9])+)([).!';/?:,][:blank])?$";
+    private static final String URL_REGEX = "^(((https?|http?)://)(%[0-9A-Fa-f]{2}|[-()_.!~*';/?:@&=+$,A-Za-z0-9])+)([).!';/?:,][:blank])?$";
+
     private String profileImageUrl;
 
 
@@ -28,9 +27,7 @@ public class ProfileImage {
     }
 
     private boolean validateUrlForm(String profileImageUrl) {
-        Pattern pattern = Pattern.compile(URL_REGEX);
-        Matcher matcher = pattern.matcher(profileImageUrl);
-        return matcher.matches();
+        return Pattern.matches(URL_REGEX, profileImageUrl);
     }
 
 }
--- be/src/test/java/com/jjikmuk/sikdorak/common/mock/OAuthMocks.java
+++ be/src/test/java/com/jjikmuk/sikdorak/common/mock/OAuthMocks.java
@@ -1,37 +1,52 @@
 package com.jjikmuk.sikdorak.common.mock;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.io.IOException;
-
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static java.nio.charset.Charset.defaultCharset;
 import static org.springframework.util.StreamUtils.copyToString;
 
+import java.io.IOException;
+import java.io.InputStream;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
 public class OAuthMocks {
-    public static void setupMockTokenResponse() throws IOException {
-        stubFor(WireMock.post(WireMock.urlEqualTo("/oauth/token?grant_type=authorization_code&client_id=1234&redirect_uri=redirectUri&code=code"))
-                .willReturn(WireMock.aResponse()
-                        .withStatus(HttpStatus.OK.value())
-                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                        .withBody(
-                                copyToString(
-                                        OAuthMocks.class.getClassLoader().getResourceAsStream("payload/get-token-response.json"),
-                                        defaultCharset()))));
+
+    public static void setUpResponses() throws IOException {
+        setupMockTokenResponse();
+        setupMockUserInfoResponse();
     }
 
-    public static void setupMockUserInfoResponse() throws IOException{
-        stubFor(WireMock.get(WireMock.urlEqualTo("/v2/user/me"))
-                .withHeader("Authorization",equalTo("bearer accessToken"))
-                .willReturn(WireMock.aResponse()
+    public static void setupMockTokenResponse() throws IOException {
+        stubFor(post(urlEqualTo("/oauth/token?grant_type=authorization_code&client_id=1234&redirect_uri=redirectUri&code=code"))
+                .willReturn(aResponse()
                         .withStatus(HttpStatus.OK.value())
                         .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                        .withBody(
-                                copyToString(
-                                        OAuthMocks.class.getClassLoader().getResourceAsStream("payload/get-user-info-response.json"),
-                                        defaultCharset()))));
+                        .withBody(getMockResponseBodyByPath("payload/get-token-response.json"))
+                )
+        );
+    }
+
+    public static void setupMockUserInfoResponse() throws IOException {
+        stubFor(get(urlEqualTo("/v2/user/me"))
+                .withHeader("Authorization", equalTo("bearer accessToken"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(getMockResponseBodyByPath("payload/get-user-info-response.json"))
+                )
+        );
+    }
+
+    private static String getMockResponseBodyByPath(String path) throws IOException {
+        return copyToString(getMockResourceStream(path), defaultCharset());
+    }
+
+    private static InputStream getMockResourceStream(String path) {
+        return OAuthMocks.class.getClassLoader().getResourceAsStream(path);
     }
 }
