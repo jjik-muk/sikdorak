77ae4750dd317ca48dd1826a4cdbbc96aa51ad04
jinan159 <wlsdhks0423@naver.com>
2022-08-09 14:30:55 +0900

    feat: :sparkles: CORS 설정 및 테스트 코드 추가

---
+++ be/src/main/java/com/jjikmuk/sikdorak/common/config/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package com.jjikmuk.sikdorak.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Value("${webconfig.cors.allowedOrigins}")
+	private String[] allowedOrigins;
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins(allowedOrigins)
+			.allowedMethods(
+				HttpMethod.GET.name(),
+				HttpMethod.POST.name(),
+				HttpMethod.PUT.name(),
+				HttpMethod.DELETE.name()
+			);
+	}
+}
