e9f7e5341aadb769af2c7217cb68a76862a155db
SeokHo-Ham <kalejuice02@gmail.com>
2022-08-09 16:59:24 +0900

    test: :white_check_mark: 액세스 토큰 재발급 실패에 대한 인수테스트 케이스 작성

--- be/src/test/java/com/jjikmuk/sikdorak/acceptance/auth/OAuthUpdateTokenAcceptanceTest.java
+++ be/src/test/java/com/jjikmuk/sikdorak/acceptance/auth/OAuthUpdateTokenAcceptanceTest.java
@@ -7,22 +7,32 @@ import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 import com.jjikmuk.sikdorak.acceptance.InitAcceptanceTest;
+import com.jjikmuk.sikdorak.auth.controller.response.JwtTokenResponse;
+import com.jjikmuk.sikdorak.auth.domain.JwtProvider;
+import com.jjikmuk.sikdorak.common.exception.ExceptionCodeAndMessages;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 @DisplayName("OAuth 토큰 재발급 인수테스트")
 public class OAuthUpdateTokenAcceptanceTest extends InitAcceptanceTest {
 
+    @Autowired
+    private JwtProvider jwtProvider;
     @Test
     @DisplayName("유저로부터 액세스 토큰 갱신 요청이 올 경우 새로운 액세스 토큰을 발급한다")
-    void update_access_token() {
+    void update_access_token_success() {
+
+
+        JwtTokenResponse tokenResponse = jwtProvider.createTokenResponse(testData.user.getUniqueId().toString());
+
         given(this.spec)
             .filter(document(DEFAULT_RESTDOC_PATH,
                 UPDATE_ACCESS_TOKEN_REQUEST_SNIPPET,
                 UPDATE_ACCESS_TOKEN_RESPONSE_SNIPPET))
-            .header("Cookie", "RefreshToken=refreshToken")
+            .header("Cookie", "RefreshToken=" + tokenResponse.getRefreshToken())
             .accept(MediaType.APPLICATION_JSON_VALUE)
 
         .when()
@@ -31,6 +41,24 @@ public class OAuthUpdateTokenAcceptanceTest extends InitAcceptanceTest {
         .then()
             .statusCode(HttpStatus.OK.value())
             .body("data.accessToken", notNullValue());
+    }
+
+    @Test
+    @DisplayName("유저로부터 액세스 토큰 갱신 요청의 리프레쉬 토큰이 유효하지 않을 경우 실패 메세지를 전송한다.")
+    void update_access_token_fail() {
+        given(this.spec)
+            .filter(document(DEFAULT_RESTDOC_PATH,
+                UPDATE_ACCESS_TOKEN_REQUEST_SNIPPET,
+                UPDATE_ACCESS_TOKEN_RESPONSE_SNIPPET))
+            .header("Cookie", "RefreshToken=InvalidToken")
+            .accept(MediaType.APPLICATION_JSON_VALUE)
+
+        .when()
+            .get("/api/oauth/refresh")
+
+        .then()
+            .statusCode(HttpStatus.BAD_REQUEST.value());
+//            .body("code", ExceptionCodeAndMessages.);
     }
 
 }
