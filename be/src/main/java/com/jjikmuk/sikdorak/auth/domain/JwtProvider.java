49fb0b085ba4f897bfa9d0fa171be9bb0ccea2f4
SeokHo-Ham <kalejuice02@gmail.com>
2022-08-09 19:57:20 +0900

    feat: :sparkles: JwtProvider 토큰 검증 로직 추가

--- be/src/main/java/com/jjikmuk/sikdorak/auth/domain/JwtProvider.java
+++ be/src/main/java/com/jjikmuk/sikdorak/auth/domain/JwtProvider.java
@@ -1,63 +1,76 @@
 package com.jjikmuk.sikdorak.auth.domain;
 
 import com.jjikmuk.sikdorak.auth.controller.response.JwtTokenResponse;
+import com.jjikmuk.sikdorak.auth.exception.InvalidTokenException;
 import com.jjikmuk.sikdorak.common.properties.JwtProperties;
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
-import java.util.Objects;
 
 @Component
-@RequiredArgsConstructor
 public class JwtProvider {
 
     private final JwtProperties jwtProperties;
+    private final SecretKey secretKey;
+
+    public JwtProvider(JwtProperties jwtProperties) {
+        this.jwtProperties = jwtProperties;
+        this.secretKey = Keys.hmacShaKeyFor(jwtProperties.getSecretKey());
+    }
 
     public JwtTokenResponse createTokenResponse(String payload) {
-        SecretKey secretKey = Keys.hmacShaKeyFor(jwtProperties.getSecretKey());
-
-        Date accessTokeExpiredDate = new Date(new Date().getTime() + jwtProperties.getAccessTokenExpiredMillisecond());
-        Date refreshTokeExpiredDate = new Date(new Date().getTime() + jwtProperties.getRefreshTokenExpiredMillisecond());
-
-        String accessToken = creatToken(payload, secretKey, accessTokeExpiredDate);
-        String refreshToken = creatToken(payload, secretKey, refreshTokeExpiredDate);
+        String accessToken = createAccessToken(payload);
+        String refreshToken = createRefreshToken(payload);
 
         return new JwtTokenResponse(accessToken, refreshToken);
     }
 
+    public String createAccessToken(String payload) {
+        Date accessTokenExpiredDate = new Date(
+            new Date().getTime() + jwtProperties.getAccessTokenExpiredMillisecond());
 
-    public boolean validateToken(String token) {
-        SecretKey secretKey = Keys.hmacShaKeyFor(jwtProperties.getSecretKey());
+        return buildToken(payload, accessTokenExpiredDate);
+    }
 
-        if (Objects.isNull(token) || token.isEmpty()) {
-//            throw new InvalidTokenException();
-        }
+    public String createRefreshToken(String payload) {
+        Date refreshTokeExpiredDate = new Date(
+            new Date().getTime() + jwtProperties.getRefreshTokenExpiredMillisecond());
 
+        return buildToken(payload, refreshTokeExpiredDate);
+    }
+
+    // INFO: token값에 대한 null 예외처리는 parseClaimsJws에서 모두 하기 때문에 별도로 작성하지 않았습니다.
+    public void validateToken(String token) {
         try {
-            Jws<Claims> claimsJws = Jwts.parserBuilder()
-                    .setSigningKey(secretKey)
-                    .build()
-                    .parseClaimsJws(token);
-            return claimsJws.getBody().getExpiration().after(new Date());
-        } catch (JwtException e) {
-            return false;
+            Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token);
+        }catch (JwtException e) {
+            throw new InvalidTokenException();
         }
     }
 
-    private static String creatToken(String payload, SecretKey secretKey, Date tokenExpiredDate) {
-        return Jwts.builder()
-                .claim("id", payload)
-                .setExpiration(tokenExpiredDate)
-                .signWith(secretKey)
-                .compact();
+    public String decodeToken(String refreshToken) {
+        Claims claim = Jwts.parserBuilder()
+            .setSigningKey(secretKey)
+            .build()
+            .parseClaimsJws(refreshToken)
+            .getBody();
+
+        return claim.get("id", String.class);
     }
 
-
+    private String buildToken(String payload, Date expiredDate) {
+        return Jwts.builder()
+            .claim("id", payload)
+            .setExpiration(expiredDate)
+            .signWith(secretKey)
+            .compact();
+    }
 }
