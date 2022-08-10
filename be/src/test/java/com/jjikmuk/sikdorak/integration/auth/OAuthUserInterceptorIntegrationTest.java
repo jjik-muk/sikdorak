3f99fbe5aa2375dd7408b10fceff12a4c63a1e84
SeokHo-Ham <kalejuice02@gmail.com>
2022-08-10 19:09:55 +0900

    test: :white_check_mark: OAuth 인터셉터 통합테스트 작성 및 통과

---
+++ be/src/test/java/com/jjikmuk/sikdorak/integration/auth/OAuthUserInterceptorIntegrationTest.java
@@ -0,0 +1,49 @@
+package com.jjikmuk.sikdorak.integration.auth;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.jjikmuk.sikdorak.auth.exception.InvalidTokenException;
+import com.jjikmuk.sikdorak.auth.interceptor.OAuthUserInterceptor;
+import com.jjikmuk.sikdorak.integration.InitIntegrationTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+@DisplayName("OAuth 인터셉터 통합 테스트")
+public class OAuthUserInterceptorIntegrationTest extends InitIntegrationTest {
+
+    private MockHttpServletRequest httpServletRequest;
+
+    @Autowired
+    private OAuthUserInterceptor oAuthUserInterceptor;
+
+    @BeforeEach
+    void setUp() {
+        this.httpServletRequest = new MockHttpServletRequest();
+    }
+
+    @Test
+    @DisplayName("정상적인 토큰이 들어오면 true를 반환하고 request에 유저의 Id를 추가한다.")
+    void oAuth_interceptor_success() throws Exception {
+        String accessToken = testData.validAccessToken;
+        httpServletRequest.addHeader("Authorization", "bearer " + accessToken);
+
+        boolean result = oAuthUserInterceptor.preHandle(httpServletRequest, null, null);
+
+        assertThat(result).isTrue();
+        assertThat(httpServletRequest.getAttribute("userId")).isEqualTo(testData.user.getId());
+    }
+
+    @Test
+    @DisplayName("유효하지 않은 토큰이 들어오면 예외를 반환한다.")
+    void oAuth_interceptor_fail() {
+        String accessToken = testData.invalidAccessToken;
+        httpServletRequest.addHeader("Authorization", "bearer " + accessToken);
+
+        assertThatThrownBy(() -> oAuthUserInterceptor.preHandle(httpServletRequest, null, null))
+            .isInstanceOf(InvalidTokenException.class);
+    }
+}
