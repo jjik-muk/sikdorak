e7706bd2a8cc1f59c8ddb185e2636ef3ca3fbea9
youryu0212 <youryu0212@naver.com>
2022-08-12 18:05:16 +0900

    SDR-108 feat: :sparkles: 댓글 좋아요 클릭시 개수 증감
    Co-authored-by: rumka <yongseongjeon@users.noreply.github.com>

--- fe/src/components/ReviewDetail/Comment/Comment.tsx
+++ fe/src/components/ReviewDetail/Comment/Comment.tsx
@@ -3,12 +3,18 @@ import WriteComment from '../WriteComment/WriteComment';
 import { Wrap, Picture, Title, Content, ContentWrapper, ButtonWrapper, Button, CommentWrapper } from './Comment.styled';
 
 const DEFAULT_IMG_URL = 'https://flyclipart.com/thumb2/profile-user-png-icon-free-download-196388.png';
-const preventEvent = (e) => {
-  e.preventDefault();
-};
 
 function Comment({ imgUrl = DEFAULT_IMG_URL, title = '식도락', content = '댓글 달아 봤습니다.' }: CommentProps) {
   const [isActiveAdditionComment, setIsActiveAdditionComment] = useState(false);
+  const [commentCnt, setCommentCnt] = useState(0);
+
+  const clickComment = (type?: string) => {
+    if (type === 'ADD') {
+      setCommentCnt(commentCnt + 1);
+      return;
+    }
+    setCommentCnt(commentCnt - 1);
+  };
 
   return (
     <Wrap>
@@ -19,7 +25,11 @@ function Comment({ imgUrl = DEFAULT_IMG_URL, title = '식도락', content = '댓
           <Content>{content}</Content>
         </ContentWrapper>
         <ButtonWrapper>
-          <Button onClick={preventEvent}>좋아요</Button>
+          {commentCnt ? (
+            <Button onClick={() => clickComment()}>{commentCnt} 좋아요</Button>
+          ) : (
+            <Button onClick={() => clickComment('ADD')}>좋아요</Button>
+          )}
           <Button onClick={handleAdditionComment}>댓글 추가</Button>
         </ButtonWrapper>
         {isActiveAdditionComment && <WriteComment />}
