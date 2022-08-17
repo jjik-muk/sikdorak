1b4bb38b939632b65f9740df5ace518152839f49
jinan159 <wlsdhks0423@naver.com>
2022-08-17 15:06:01 +0900

    SDR-117 test: :white_check_mark: 가게 삭제 API 인수테스트에 실패하는 경우 테스트 추가 #time 10m

--- be/src/test/java/com/jjikmuk/sikdorak/acceptance/store/StoreRemoveAcceptanceTest.java
+++ be/src/test/java/com/jjikmuk/sikdorak/acceptance/store/StoreRemoveAcceptanceTest.java
@@ -6,7 +6,9 @@ import static io.restassured.RestAssured.given;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
 
 import com.jjikmuk.sikdorak.acceptance.InitAcceptanceTest;
+import com.jjikmuk.sikdorak.common.CodeAndMessages;
 import com.jjikmuk.sikdorak.common.ResponseCodeAndMessages;
+import com.jjikmuk.sikdorak.common.exception.ExceptionCodeAndMessages;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
@@ -37,6 +39,30 @@ class StoreRemoveAcceptanceTest extends InitAcceptanceTest {
 
 		.then()
 			.statusCode(HttpStatus.OK)
+			.body("code", Matchers.equalTo(expectedCodeAndMessage.getCode()))
+			.body("message", Matchers.equalTo(expectedCodeAndMessage.getMessage()));
+	}
+
+	@Test
+	@DisplayName("가게 삭제 요청 시 존재하지 않는 가게에 대한 요청이라면, 삭제되지 않고 실패 메세지를 전송한다.")
+	void remove_store_failed() {
+		Long unsavedStoreId = Long.MIN_VALUE;
+
+		CodeAndMessages expectedCodeAndMessage = ExceptionCodeAndMessages.FAILED_DELETE_STORE;
+
+		given(this.spec).log().all()
+			.filter(document(DEFAULT_RESTDOC_PATH,
+				STORE_REMOVE_REQUEST_PARAM_SNIPPET,
+				STORE_REMOVE_RESPONSE_SNIPPET))
+			.accept(MediaType.APPLICATION_JSON_VALUE)
+			.header("Authorization", testData.userValidAuthorizationHeader)
+			.contentType(ContentType.JSON)
+
+			.when()
+			.delete("/api/stores/{storeId}", unsavedStoreId)
+
+			.then()
+			.statusCode(HttpStatus.BAD_REQUEST)
 			.body("code", Matchers.equalTo(expectedCodeAndMessage.getCode()))
 			.body("message", Matchers.equalTo(expectedCodeAndMessage.getMessage()));
 	}
