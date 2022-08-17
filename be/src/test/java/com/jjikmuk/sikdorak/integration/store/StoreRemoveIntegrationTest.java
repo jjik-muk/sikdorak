4d9a175800c0ccb4e15a453c6ee3df33c4e5dd64
jinan159 <wlsdhks0423@naver.com>
2022-08-17 17:25:08 +0900

    SDR-117 feat :sparkle: Store 삭제 서비스를 soft deletion 으로 변경 #time 20m
    - StoreService.removeStore 를 soft deletion 으로 변경
    - 조회 로직에서 삭제된 store 조회되지 않도록 변경

--- be/src/test/java/com/jjikmuk/sikdorak/integration/store/StoreRemoveIntegrationTest.java
+++ be/src/test/java/com/jjikmuk/sikdorak/integration/store/StoreRemoveIntegrationTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.jjikmuk.sikdorak.integration.InitIntegrationTest;
 import com.jjikmuk.sikdorak.store.domain.Store;
-import com.jjikmuk.sikdorak.store.exception.StoreDeleteFailedException;
+import com.jjikmuk.sikdorak.store.exception.StoreNotFoundException;
 import com.jjikmuk.sikdorak.store.repository.StoreRepository;
 import com.jjikmuk.sikdorak.store.service.StoreService;
 import java.util.Optional;
@@ -48,7 +48,7 @@ public class StoreRemoveIntegrationTest extends InitIntegrationTest {
 
             // then
             assertThatThrownBy(() -> storeService.removeStore(notSavedStoreId))
-                .isInstanceOf(StoreDeleteFailedException.class);
+                .isInstanceOf(StoreNotFoundException.class);
         }
     }
 }
