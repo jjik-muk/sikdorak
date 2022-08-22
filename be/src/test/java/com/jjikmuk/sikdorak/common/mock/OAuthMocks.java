99a741476e5be8eb3d57df8cf09642a612697d45
SeokHo-Ham <kalejuice02@gmail.com>
2022-08-22 15:49:06 +0900

    SDR-174 BE - fix : 카카오 로그인 통합 테스트 작성

--- be/src/test/java/com/jjikmuk/sikdorak/common/mock/OAuthMocks.java
+++ be/src/test/java/com/jjikmuk/sikdorak/common/mock/OAuthMocks.java
@@ -6,6 +6,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
 import static java.nio.charset.Charset.defaultCharset;
 import static org.springframework.util.StreamUtils.copyToString;
 
@@ -19,6 +20,7 @@ public class OAuthMocks {
     public static void setUpResponses() throws IOException {
         setupMockTokenResponse();
         setupMockUserInfoResponse();
+        setupMockUserInfoWithNoEmailReponse();
     }
 
     public static void setupMockTokenResponse() throws IOException {
@@ -33,12 +35,27 @@ public class OAuthMocks {
 
     public static void setupMockUserInfoResponse() throws IOException {
         stubFor(get(urlEqualTo("/v2/user/me"))
+            .inScenario("Email Not Null")
+            .whenScenarioStateIs(STARTED)
                 .withHeader("Authorization", equalTo("bearer accessToken"))
                 .willReturn(aResponse()
                         .withStatus(HttpStatus.OK.value())
                         .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                         .withBody(getMockResponseBodyByPath("payload/get-user-info-response.json"))
                 )
+        );
+    }
+
+    public static void setupMockUserInfoWithNoEmailReponse() throws IOException{
+        stubFor(get(urlEqualTo("/v2/user/me"))
+            .inScenario("Email Null")
+            .whenScenarioStateIs(STARTED)
+            .withHeader("Authorization", equalTo("bearer accessToken"))
+            .willReturn(aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                .withBody(getMockResponseBodyByPath("payload/get-user-info-with-no-email-response.json"))
+            )
         );
     }
 
