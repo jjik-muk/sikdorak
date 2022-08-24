6b4cac9c2602360762e64aa7f049182737158a89
“kukim” <kukim.dev@gmail.com>
2022-08-24 11:34:29 +0900

    SDR-183 test: 리뷰 디테일 조회 인수 테스트 생성(유저가 본인 리뷰 조회한 경우)

---
+++ be/src/test/java/com/jjikmuk/sikdorak/acceptance/review/ReviewSearchAcceptanceTest.java
@@ -0,0 +1,49 @@
+package com.jjikmuk.sikdorak.acceptance.review;
+
+import static io.restassured.RestAssured.given;
+
+import com.jjikmuk.sikdorak.acceptance.InitAcceptanceTest;
+import com.jjikmuk.sikdorak.common.ResponseCodeAndMessages;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+/**
+ * 디테일 조회 가능한 사람이 조회한 경우 : public 글
+ * 조회 불가능한 사람이 조회한 경우 : 게스트/친구관계가 protected, private 조회 , 친구관계가 private 조회
+ */
+@DisplayName("리뷰조회 인수테스트")
+class ReviewSearchAcceptanceTest extends InitAcceptanceTest {
+
+	@Test
+	@DisplayName("유저가 본인 리뷰를 조회한 경우 디테일 리뷰를 보여준다.")
+	void modify_review_success() {
+		given(this.spec)
+			.log().all()
+//			.filter(
+//				document(DEFAULT_RESTDOC_PATH,
+//					REVIEW_MODIFY_REQUEST_PARAM_SNIPPET,
+//					REVIEW_MODIFY_REQUEST_SNIPPET,
+//					REVIEW_MODIFY_RESPONSE_SNIPPET))
+			.accept(MediaType.APPLICATION_JSON_VALUE)
+			.header("Content-type", "application/json")
+			.header("Authorization", testData.user1ValidAuthorizationHeader)
+
+		.when()
+			.get("/api/reviews/{reviewId}", testData.review.getId())
+
+		.then()
+			.log().all()
+			.statusCode(HttpStatus.OK.value())
+			.body("code", Matchers.equalTo(ResponseCodeAndMessages.REVIEW_SEARCH_DETAIL_SUCCESS.getCode()))
+			.body("message",
+				Matchers.equalTo(ResponseCodeAndMessages.REVIEW_SEARCH_DETAIL_SUCCESS.getMessage()));
+	}
+
+
+
+
+
+}
