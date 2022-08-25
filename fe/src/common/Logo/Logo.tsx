c3a94e2c5e76c6a83e4fefb69c96281acd2007f0
yongseongjeon <yongseong.jeon95@gmail.com>
2022-08-25 21:50:00 +0900

    SDR-101 feat: 로고 또는 홈 아이콘 클릭하면 메인 페이지로 이동

--- fe/src/common/Logo/Logo.tsx
+++ fe/src/common/Logo/Logo.tsx
@@ -1,5 +1,11 @@
+import { Link } from 'react-router-dom';
+
 function Logo() {
-  return <h1>Sikdorak</h1>;
+  return (
+    <Link to="/">
+      <h1>Sikdorak</h1>
+    </Link>
+  );
 }
 
 export default Logo;
--- fe/src/components/Common/CommonHeader.tsx
+++ fe/src/components/Common/CommonHeader.tsx
@@ -4,23 +4,23 @@ import Portal from 'common/Portal/Portal';
 import { useOutsideClick } from 'hooks/useOutsideClick';
 import useToggle from 'hooks/useToggle';
 import ReviewWrite from 'pages/ReviewWrite/ReviewWrite';
-import UserDetail from 'pages/UserDetail/userDetail';
 import { useRef, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { createKey } from 'utils/utils';
 import { ButtonWrap, IconWrap, Input, SearchFormWrap, Header, Wrap } from './CommonHeader.styled';
 
 function CommonHeader() {
   const [isReviewWrite, setIsReviewWrite] = useState(false);
-  const [isUserProfile, toggleIsUserProfile] = useToggle(false);
+  const [, toggleIsUserProfile] = useToggle(false);
   const reviewWriteModalRef = useRef(null);
   const userDetailModalRef = useRef(null);
 
   const iconInfo: IconInfoProps[] = [
-    { icon: 'Home', handler: toggleIsUserProfile },
-    { icon: 'Map' },
-    { icon: 'PostBtn', handler: handleReviewWrite },
-    { icon: 'Alarm' },
-    { icon: 'Profile' },
+    { icon: 'Home', handler: toggleIsUserProfile, to: '/' },
+    { icon: 'Map', to: '/map' },
+    { icon: 'PostBtn', handler: handleReviewWrite, to: '' },
+    { icon: 'Alarm', to: '' },
+    { icon: 'Profile', to: '/userDetail' },
   ];
 
   useOutsideClick(reviewWriteModalRef, handleReviewWrite);
@@ -37,20 +37,17 @@ function CommonHeader() {
           </IconWrap>
         </SearchFormWrap>
         <ButtonWrap>
-          {iconInfo.map(({ icon, handler }, idx) => (
-            <div key={createKey(icon, idx)} onClick={handler}>
-              <Icon icon={icon} width={24} height={24} />
-            </div>
+          {iconInfo.map(({ icon, handler, to }, idx) => (
+            <Link to={to}>
+              <div key={createKey(icon, idx)} onClick={handler}>
+                <Icon icon={icon} width={24} height={24} />
+              </div>
+            </Link>
           ))}
         </ButtonWrap>
         {isReviewWrite && (
           <Portal selector="#portal" ref={reviewWriteModalRef}>
             <ReviewWrite />
-          </Portal>
-        )}
-        {isUserProfile && (
-          <Portal selector="#portal" ref={userDetailModalRef}>
-            <UserDetail />
           </Portal>
         )}
       </Header>
@@ -65,6 +62,7 @@ function CommonHeader() {
 type IconInfoProps = {
   icon: IconComponentsKeys;
   handler?: React.MouseEventHandler<HTMLDivElement>;
+  to?: string;
 };
 
 export default CommonHeader;
