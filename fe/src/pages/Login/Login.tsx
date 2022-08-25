3487f821fcf1d0bbcf8a267693fbd3b3a9509b93
yongseongjeon <yongseong.jeon95@gmail.com>
2022-08-25 09:51:04 +0900

    SDR-190 feat: accessToken이 있으면 Feed View로 이동

--- fe/src/pages/Login/Login.tsx
+++ fe/src/pages/Login/Login.tsx
@@ -1,3 +1,4 @@
+import { DOMAIN } from 'constants/dummyData';
 import TEXT from 'constants/text';
 import Logo from 'common/Logo/Logo';
 import Portal from 'common/Portal/Portal';
@@ -5,22 +6,23 @@ import CommonHeader from 'components/Common/CommonHeader';
 import LoginButton from 'components/Login/LoginButton/LoginButton';
 import LoginInput from 'components/Login/LoginInput/LoginInput';
 import { useOutsideClick } from 'hooks/useOutsideClick';
+import useToggle from 'hooks/useToggle';
 import ReviewDetail from 'pages/ReviewDetail/ReviewDetail';
-import { useRef, useState } from 'react';
-import { Form, Wrap } from './Login.styled';
+import { useRef } from 'react';
+import { Navigate } from 'react-router-dom';
+import { Form, KakaoLogin, Wrap } from './Login.styled';
 
 const { KAKAO, NORMAL } = TEXT.LOGIN_BTN;
 const { ID, PASSWORD } = TEXT.INPUT;
 
 function Login() {
-  const [isPortal, setIsPortal] = useState(false);
+  const [isPortal, toggleIsPortal] = useToggle(false);
   const modalRef = useRef(null);
+  useOutsideClick(modalRef, toggleIsPortal);
 
-  const handleClick = () => {
-    setIsPortal(!isPortal);
-  };
-
-  useOutsideClick(modalRef, handleClick);
+  if (localStorage.getItem('accessToken')) {
+    return <Navigate to="/reviewList" />;
+  }
 
   return (
     <>
@@ -28,10 +30,12 @@ function Login() {
       <Wrap>
         <Form>
           <Logo />
-          <LoginButton text={KAKAO} />
+          <KakaoLogin href={`${DOMAIN}/api/oauth/login`}>
+            <LoginButton text={KAKAO} />
+          </KakaoLogin>
           <LoginInput type={ID} />
           <LoginInput type={PASSWORD} />
-          <LoginButton text={NORMAL} onClick={handleClick} />
+          <LoginButton text={NORMAL} onClick={toggleIsPortal} />
           <span>회원가입</span>
           {isPortal && (
             <Portal selector="#portal" ref={modalRef}>
