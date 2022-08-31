9f03224c1a09a572d612136db444d8563dfc451c
SeokHo-Ham <kalejuice02@gmail.com>
2022-08-31 12:17:45 +0900

    SDR-209 refactor: 쿼리에 text blocks 적용

--- be/src/main/java/com/jjikmuk/sikdorak/review/domain/RecommendationType.java
+++ be/src/main/java/com/jjikmuk/sikdorak/review/domain/RecommendationType.java
@@ -11,9 +11,11 @@ public enum RecommendationType {
     /**
      * @deprecated (서버에서 자체적으로 추천 조건을 설정하도록 수정하도록 변경됨)
      */
-
+    @Deprecated(since = "1.0", forRemoval = true)
     TIME("created_at"),
+    @Deprecated(since = "1.0", forRemoval = true)
     LIKE("like"),
+    @Deprecated(since = "1.0", forRemoval = true)
     SCORE("review_score");
 
     private String orderType;
--- be/src/main/java/com/jjikmuk/sikdorak/review/repository/ReviewRepository.java
+++ be/src/main/java/com/jjikmuk/sikdorak/review/repository/ReviewRepository.java
@@ -9,27 +9,31 @@ import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
-	List<Review> findByUserId(Long userId);
+    List<Review> findByUserId(Long userId);
 
-	@Query("select r from Review as r where r.userId = :userId and not r.reviewVisibility = 'PRIVATE'")
-	List<Review> findByUserIdAndConnection(@Param("userId")Long userId);
+    @Query("select r from Review as r where r.userId = :userId and not r.reviewVisibility = 'PRIVATE'")
+    List<Review> findByUserIdAndConnection(@Param("userId") Long userId);
 
-	@Query("select r from Review as r where r.userId = :userId and r.reviewVisibility = 'PUBLIC'")
-	List<Review> findByUserIdAndDisconnection(@Param("userId")Long userId);
+    @Query("select r from Review as r where r.userId = :userId and r.reviewVisibility = 'PUBLIC'")
+    List<Review> findByUserIdAndDisconnection(@Param("userId") Long userId);
 
-	Integer countByUserId(Long userId);
+    Integer countByUserId(Long userId);
 
-	@Query("select r from Review r "
-		+ "where r.reviewVisibility = 'PUBLIC' and r.id < :targetId "
-		+ "order by r.createdAt desc")
+    @Query("""
+        select r from Review r
+        where r.reviewVisibility = 'PUBLIC' and r.id < :targetId
+        order by r.createdAt desc
+        """)
     List<Review> findPublicRecommendedReviewsInRecentOrder(
-		@Param("targetId") long targetId ,
-		Pageable pageable);
+        @Param("targetId") long targetId,
+        Pageable pageable);
 
-	@Query("select r from Review r "
-		+ "where r.reviewVisibility = 'PROTECTED' or r.reviewVisibility = 'PUBLIC' and r.id < :targetId "
-		+ "order by field(r.reviewVisibility, 'PROTECTED','PUBLIC'),r.createdAt desc")
-	List<Review> findPublicAndProtectedRecommendedReviewsInRecentOrder(
-		@Param("targetId") long targetId ,
-		Pageable pageable);
+    @Query("""
+        	select r from Review r
+        	where r.reviewVisibility = 'PROTECTED' or r.reviewVisibility = 'PUBLIC' and r.id < :targetId
+        	order by field(r.reviewVisibility, 'PROTECTED','PUBLIC'),r.createdAt desc
+        """)
+    List<Review> findPublicAndProtectedRecommendedReviewsInRecentOrder(
+        @Param("targetId") long targetId,
+        Pageable pageable);
 }
