357214cef124cd5722a5d17c78fa4bc82827511b
yongseongjeon <yongseong.jeon95@gmail.com>
2022-09-01 17:51:10 +0900

    SDR-101 feat: 스크롤 맨 아래로 내리는 이벤트 감지

--- fe/src/pages/ReviewList/ReviewList.styled.ts
+++ fe/src/pages/ReviewList/ReviewList.styled.ts
@@ -4,3 +4,9 @@ export const Wrap = styled.div`
   width: fit-content;
   margin: 0 auto;
 `;
+
+export const ReviewListWrap = styled.div`
+  width: 100%;
+  height: 1000px;
+  overflow-y: scroll;
+`;
--- fe/src/pages/ReviewList/ReviewList.tsx
+++ fe/src/pages/ReviewList/ReviewList.tsx
@@ -1,15 +1,15 @@
-import { DOMAIN } from 'constants/dummyData';
+import { DOMAIN, FEEDS } from 'constants/dummyData';
 import CommonHeader from 'components/CommonHeader/CommonHeader';
 import Feed from 'components/ReviewList/Feed/Feed';
 import { useEffect, useId, useState } from 'react';
 import { createKey, fetchDataThatNeedToLogin } from 'utils/utils';
-import { Wrap } from './ReviewList.styled';
+import { ReviewListWrap, Wrap } from './ReviewList.styled';
 
 function ReviewList() {
   const id = useId();
   const [reviews, setReviews] = useState([]);
   const [afterParam, setAfterParam] = useState(0);
-  const REVIEW_SIZE = 5;
+  const REVIEW_SIZE = 1;
 
   useEffect(() => {
     const hasNextPage = afterParam !== 1;
@@ -28,23 +28,30 @@ function ReviewList() {
   }, []);
 
   return (
-    <>
+    <ReviewListWrap onScroll={handleScroll}>
       <CommonHeader />
       <Wrap>
-        {reviews &&
-          reviews.map(({ author, contents, pictures, store, likeCnt }, i) => (
-            <Feed
-              key={createKey(id, i)}
-              author={author}
-              contents={contents}
-              pictures={pictures}
-              store={store}
-              likeCnt={likeCnt}
-            />
-          ))}
+        {FEEDS.map(({ author, contents, pictures, store, likeCnt }, i) => (
+          <Feed
+            key={createKey(id, i)}
+            author={author}
+            contents={contents}
+            pictures={pictures}
+            store={store}
+            likeCnt={likeCnt}
+          />
+        ))}
       </Wrap>
-    </>
+    </ReviewListWrap>
   );
+
+  function handleScroll(e) {
+    const { scrollHeight, scrollTop, clientHeight } = e.target as HTMLDivElement;
+    const isScrollEnd = scrollHeight - scrollTop === clientHeight;
+    if (isScrollEnd) {
+      console.log('끝');
+    }
+  }
 }
 
 export default ReviewList;
