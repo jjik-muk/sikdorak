68d35c2c04bfa18dfda949038c53640a1d5118c3
yongseongjeon <yongseong.jeon95@gmail.com>
2022-09-02 17:22:45 +0900

    SDR-101 feat: 유저 평점 회색 별도 표시

--- fe/src/components/ReviewDetail/TotalRating/Rating.tsx
+++ fe/src/components/ReviewDetail/TotalRating/Rating.tsx
@@ -2,12 +2,26 @@ import Icon from 'components/Common/Icon/Icon';
 import { Wrap } from './Rating.styled';
 
 function Rating({ rating }: TotalRatingProps) {
+  const yellowStarLen = rating;
+  const greyStarLen = 5 - rating;
+  const YELLOW = '#f1c40f';
+  const GREY = '#bdc3c7';
+
   return (
     <Wrap>
-      {Array.from({ length: rating }).map(() => (
-        <Icon icon="Star" stroke="#fff" fill="#f1c40f" width={15} height={15} />
-      ))}
+      <Star length={yellowStarLen} color={YELLOW} />
+      <Star length={greyStarLen} color={GREY} />
     </Wrap>
+  );
+}
+
+function Star({ length, color }: StarProps) {
+  return (
+    <>
+      {Array.from({ length }).map(() => (
+        <Icon icon="Star" stroke="#fff" fill={color} width={15} height={15} />
+      ))}
+    </>
   );
 }
 
@@ -15,4 +29,9 @@ export default Rating;
 
 type TotalRatingProps = {
   rating: number;
+};
+
+type StarProps = {
+  length: number;
+  color: string;
 };
