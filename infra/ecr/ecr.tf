09fbba01a697ef6308b65ef43825199100424b8a
“kukim” <kukim.dev@gmail.com>
2022-09-05 20:23:08 +0900

    SDR-261 ECR lifecycle 정책 추가 #time 30m

--- infra/ecr/ecr.tf
+++ infra/ecr/ecr.tf
@@ -6,3 +6,9 @@ resource "aws_ecr_repository" "ecr_springboot" {
     scan_on_push = false
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "ecr_policy_springboot" {
+  repository = aws_ecr_repository.ecr_springboot.name
+
+  policy = file("./json-policies/lifecyclepolicy.json")
+}
