3b7332e41183daf221cd911dd31c9ff1eaa37e09
“kukim” <kukim.dev@gmail.com>
2022-09-07 16:19:30 +0900

    SDR-55 RDS 프로비저닝 #time 4h 30m
    - 인스턴스 생성
    - 관리자 id, 비밀번호 입력 받아 사용
    - 보안그룹 지정
    - 커스텀  파라미터그룹 생성과 지정

--- infra/rds/rds.tf
+++ infra/rds/rds.tf
@@ -1,12 +1,13 @@
 # TODO
 # [x] subnet group
-# [] db instance
-# [] parameter group
-# [] input : db id/pw  variable
-# [] ouput varibale : hostname, port
+# [x] db instance
+# [x] parameter group
+# [x] input : db id/pw  variable
+# [x] ouput varibale : hostname, port
 # [] +a) read replica 
 # ref : https://learn.hashicorp.com/tutorials/terraform/aws-rds?in=terraform/aws
 
+# subnet group : private subnet으로 구성된 RDS에 접근할 수 있는 서브넷 목록 입니다.
 resource "aws_db_subnet_group" "db_subnet_group_sikdorak" {
 
   name = "db_subnet_sikdorak"
@@ -21,4 +22,69 @@ resource "aws_db_subnet_group" "db_subnet_group_sikdorak" {
     Name = "be"
   }
 
+}
+
+resource "aws_db_instance" "db_sidkorak" {
+
+  identifier        = "sikdorak-mysql"
+  instance_class    = "db.t4g.micro" # free tier
+  allocated_storage = 20
+  engine            = "mysql"
+  engine_version    = "8.0.28"
+  db_name              = "sikdorak_db"
+  username          = var.db_username
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group_sikdorak.name
+  vpc_security_group_ids = [aws_security_group.security_group_db.id]
+  parameter_group_name   = aws_db_parameter_group.korea_v1.name
+  publicly_accessible    = false
+  skip_final_snapshot    = false
+
+}
+
+resource "aws_db_parameter_group" "korea_v1" {
+  name   = "rds-mydql"
+  family = "mysql8.0"
+
+  parameter {
+    name  = "collation_server"
+    value = "utf8mb4_general_ci"
+  }
+
+  parameter {
+    name  = "collation_server"
+    value = "utf8mb4_general_ci"
+  }
+
+  parameter {
+    name  = "character_set_client"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_server"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "character_set_connection"
+    value = "utf8mb4"
+  }
+  parameter {
+    name  = "character_set_database"
+    value = "utf8mb4"
+  }
+  parameter {
+    name  = "character_set_filesystem"
+    value = "utf8mb4"
+  }
+  parameter {
+    name  = "character_set_results"
+    value = "utf8mb4"
+  }
+
+  parameter {
+    name  = "time_zone"
+    value = "Asia/Seoul"
+  }
 }
