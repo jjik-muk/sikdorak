8e4004f544c5249621197ba1fc19a45da7d766f2
jinan159 <wlsdhks0423@naver.com>
2022-09-07 15:30:11 +0900

    SDR-259: test: 카카오 로컬 API Mock 리펙토링
    - 시나리오 없이 stub 설정되도록 변경
    - stub 초기화 로직 개선

--- be/src/test/java/com/jjikmuk/sikdorak/common/mock/KakaoPlaceMocks.java
+++ be/src/test/java/com/jjikmuk/sikdorak/common/mock/KakaoPlaceMocks.java
@@ -6,7 +6,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.stubbing.Scenario;
+import com.github.tomakehurst.wiremock.stubbing.StubMapping;
 import com.jjikmuk.sikdorak.common.TestResourceUtils;
 import org.apache.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,59 +15,43 @@ public abstract class KakaoPlaceMocks {
 
 	private final static String PLACE_KEYWORD_SEARCH_URL = "/v2/local/search/keyword.json";
 
-	private static final String SCENARIO_KEYWORD_SEARCH = "place_keyword_search_url";
-
-	private final static String PLACE_RESPONSE_BODY = TestResourceUtils.getTestResourceWithPath("payload/get-kakao-search-place-response.json");
+	private final static String PLACE_KEYWORD_SEARCH_RESPONSE_BODY = TestResourceUtils.getTestResourceWithPath("payload/get-kakao-search-place-response.json");
 
 	private final static String ADDRESS_SEARCH_URL = "/v2/local/search/address.json";
 
-	private static final String SCENARIO_ADDRESS_SEARCH = "address_search_url";
+	private final static String ADDRESS_SEARCH_RESPONSE_BODY = TestResourceUtils.getTestResourceWithPath("payload/get-kakao-search-address-response.json");
 
-	private final static String ADDRESS_RESPONSE_BODY = TestResourceUtils.getTestResourceWithPath("payload/get-kakao-search-address-response.json");
+	private static StubMapping placeKeywordSearchStub;
+
+	private static StubMapping addressSearchStub;
 
 	public static void startAllMocks() {
-		startSearchPlacesMockScenario();
-		startSearchAddressMockScenario();
+		setupSearchPlacesStub();
+		setupSearchAddressStub();
 	}
 
 	public static void resetAllMocks() {
-		WireMock.getAllScenarios()
-			.forEach(scenario -> WireMock.resetScenario(scenario.getId()));
+		WireMock.removeStub(placeKeywordSearchStub);
+		WireMock.removeStub(addressSearchStub);
 	}
 
-	public static void startSearchPlacesMockScenario() {
-		stubFor(get(urlPathEqualTo(PLACE_KEYWORD_SEARCH_URL))
-			.inScenario(SCENARIO_KEYWORD_SEARCH)
-			.whenScenarioStateIs(Scenario.STARTED)
+	public static void setupSearchPlacesStub() {
+		placeKeywordSearchStub = stubFor(get(urlPathEqualTo(PLACE_KEYWORD_SEARCH_URL))
 			.willReturn(aResponse()
 				.withStatus(HttpStatus.OK.value())
 				.withHeader(HttpHeaders.CONTENT_TYPE, "application/json; charset=utf-8")
-				.withBody(PLACE_RESPONSE_BODY)
+				.withBody(PLACE_KEYWORD_SEARCH_RESPONSE_BODY)
 			)
 		);
-
-		WireMock.setScenarioState(SCENARIO_KEYWORD_SEARCH, Scenario.STARTED);
 	}
 
-	public static void resetSearchPlacesMockScenario() {
-		WireMock.resetScenario(SCENARIO_KEYWORD_SEARCH);
-	}
-
-	public static void startSearchAddressMockScenario() {
-		stubFor(get(urlPathEqualTo(ADDRESS_SEARCH_URL))
-			.inScenario(SCENARIO_ADDRESS_SEARCH)
-			.whenScenarioStateIs(Scenario.STARTED)
+	public static void setupSearchAddressStub() {
+		addressSearchStub = stubFor(get(urlPathEqualTo(ADDRESS_SEARCH_URL))
 			.willReturn(aResponse()
 				.withStatus(HttpStatus.OK.value())
 				.withHeader(HttpHeaders.CONTENT_TYPE, "application/json; charset=utf-8")
-				.withBody(ADDRESS_RESPONSE_BODY)
+				.withBody(ADDRESS_SEARCH_RESPONSE_BODY)
 			)
 		);
-
-		WireMock.setScenarioState(SCENARIO_ADDRESS_SEARCH, Scenario.STARTED);
-	}
-
-	public static void resetSearchAddressMockScenario() {
-		WireMock.resetScenario(SCENARIO_ADDRESS_SEARCH);
 	}
 }
