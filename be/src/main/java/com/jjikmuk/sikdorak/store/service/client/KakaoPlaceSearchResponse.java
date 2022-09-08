e393ea03026e962afc56ad01a6b22c2903d795a7
jinan159 <wlsdhks0423@naver.com>
2022-09-08 15:17:21 +0900

    SDR-259: refactor: 카카오 API 응답 클래스 생성자 정리
    - 불필요한 lombok 생성자 어노테이션 제거
    - List 반환시, unmodifiableList 로 반환하도록 변경
      - lombok Getter 제거

--- be/src/main/java/com/jjikmuk/sikdorak/store/service/client/KakaoPlaceSearchResponse.java
+++ be/src/main/java/com/jjikmuk/sikdorak/store/service/client/KakaoPlaceSearchResponse.java
@@ -1,14 +1,13 @@
 package com.jjikmuk.sikdorak.store.service.client;
 
+import java.util.Collections;
 import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
 public class KakaoPlaceSearchResponse {
 
 	List<KakaoPlaceResponse> documents;
+
+	public List<KakaoPlaceResponse> getDocuments() {
+		return Collections.unmodifiableList(documents);
+	}
 }
--- be/src/main/java/com/jjikmuk/sikdorak/store/service/client/KakaoRoadAddressResponse.java
+++ be/src/main/java/com/jjikmuk/sikdorak/store/service/client/KakaoRoadAddressResponse.java
@@ -3,10 +3,8 @@ package com.jjikmuk.sikdorak.store.service.client;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KakaoRoadAddressResponse {
 
