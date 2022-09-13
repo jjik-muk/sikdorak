129d8045821edcd611265174c88ad5e6c6db01c9
jinan159 <wlsdhks0423@naver.com>
2022-09-13 21:13:09 +0900

    SDR-273: test: 가게 리뷰 목록 조회시, PUBLIC 리뷰만 조회되는지에 대한 테스트 추가

--- be/src/test/java/com/jjikmuk/sikdorak/integration/review/ReviewsSearchByStoreIdIntegrationTest.java
+++ be/src/test/java/com/jjikmuk/sikdorak/integration/review/ReviewsSearchByStoreIdIntegrationTest.java
@@ -8,6 +8,7 @@ import com.jjikmuk.sikdorak.common.controller.request.CursorPageRequest;
 import com.jjikmuk.sikdorak.integration.InitIntegrationTest;
 import com.jjikmuk.sikdorak.review.controller.response.ReviewListResponse;
 import com.jjikmuk.sikdorak.review.controller.response.reviewdetail.ReviewDetailResponse;
+import com.jjikmuk.sikdorak.review.domain.ReviewVisibility;
 import com.jjikmuk.sikdorak.review.service.ReviewService;
 import com.jjikmuk.sikdorak.store.domain.Store;
 import com.jjikmuk.sikdorak.store.exception.NotFoundStoreException;
@@ -29,7 +30,7 @@ class ReviewsSearchByStoreIdIntegrationTest extends InitIntegrationTest {
 
         @Test
         @DisplayName("가게가 존재하는 가게라면 리뷰 목록이 반환된다.")
-        void exactly_same_store_name_returns_store_list() {
+        void reviews_of_store_search_success() {
             // given
             Store store = testData.store;
             CursorPageRequest pageRequest = new CursorPageRequest(0L, 0L, 10, true);
@@ -46,15 +47,16 @@ class ReviewsSearchByStoreIdIntegrationTest extends InitIntegrationTest {
                 .allSatisfy(reviewDetail ->
                     assertAll(
                         () -> assertThat(reviewDetail.store().storeId()).isEqualTo(store.getId()),
-                        () -> assertThat(reviewDetail.store().storeName()).isEqualTo(
-                            store.getStoreName())
+                        () -> assertThat(reviewDetail.store().storeName()).isEqualTo(store.getStoreName()),
+                        () -> assertThat(reviewDetail.store().addressName()).isEqualTo(store.getAddressName()),
+                        () -> assertThat(reviewDetail.store().roadAddressName()).isEqualTo(store.getRoadAddressName())
                     )
                 );
         }
 
         @Test
         @DisplayName("가게가 존재하지 않으면 예외를 발생킨다.")
-        void partially_same_store_name_returns_store_list() {
+        void reviews_of_store_search_failed() {
             // given
             long notExistingStoreId = Long.MIN_VALUE;
             CursorPageRequest pageRequest = new CursorPageRequest(0L, 0L, 10, true);
@@ -63,6 +65,27 @@ class ReviewsSearchByStoreIdIntegrationTest extends InitIntegrationTest {
             assertThatThrownBy(
                 () -> reviewService.searchReviewsByStoreId(notExistingStoreId, pageRequest))
                 .isInstanceOf(NotFoundStoreException.class);
+        }
+
+        @Test
+        @DisplayName("리뷰는 PUBLIC 인 항목들만 반환된다.")
+        void reviews_of_store_search_only_public_success() {
+            // given
+            Store store = testData.store;
+            CursorPageRequest pageRequest = new CursorPageRequest(0L, 0L, 10, true);
+
+            // when
+            ReviewListResponse reviewResponse = reviewService.searchReviewsByStoreId(
+                store.getId(), pageRequest);
+
+            // then
+            assertThat(reviewResponse).isNotNull();
+
+            List<ReviewDetailResponse> reviews = reviewResponse.reviews();
+            assertThat(reviews).isNotEmpty()
+                .allSatisfy(reviewDetail ->
+                    assertThat(reviewDetail.reviewVisibility())
+                        .isEqualTo(ReviewVisibility.PUBLIC.name()));
         }
     }
 }
