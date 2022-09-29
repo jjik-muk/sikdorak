864c21608a5c3105fe3e27d5b19ab4fc2f2ee7b3
yongseongjeon <yongseong.jeon95@gmail.com>
2022-09-29 20:00:22 +0900

    [SDR-364] FE : MUI 설치

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -12,6 +12,10 @@ import StoreDetail from 'pages/StoreDetail/StoreDetail';
 import UserDetail from 'pages/UserDetail/UserDetail';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import GlobalStyle from 'styles/GlobalStyle';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
