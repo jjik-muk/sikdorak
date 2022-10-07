8c2dc186038bb93ba09b1adbddf4f579128f8dee
“kukim” <kukim.dev@gmail.com>
2022-10-07 14:50:24 +0700

    SDR-331 docs: initImageMetaData 메서드 주석

--- be/src/main/java/com/jjikmuk/sikdorak/image/command/app/ImageMetaDataServiceImpl.java
+++ be/src/main/java/com/jjikmuk/sikdorak/image/command/app/ImageMetaDataServiceImpl.java
@@ -18,6 +18,14 @@ public class ImageMetaDataServiceImpl implements ImageMetaDataService {
 	private final UserRepository userRepository;
 	private final ImageMetaDataRepository imageMetaDataRepository;
 
+	/**
+	 * 이미지 메타 데이터 초기 생성을 합니다.
+	 * 초기 image 데이터 사이즈는 0KB로 초기화 됩니다. 메타데이터 크기는 리뷰 작성, 수정할 떄 수정됩니다.
+	 *
+	 * @param fileName s3에 저장된 이미지 파일 이름
+	 * @param loginUser 유저
+	 * @return ImageMetaData 객체
+	 */
 	@Override
 	@Transactional
 	public ImageMetaData initImageMetaData(String fileName, LoginUser loginUser) {
