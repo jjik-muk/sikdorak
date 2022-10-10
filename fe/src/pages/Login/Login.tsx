110d1cf5dafa565c0c4cb9f446ec71015a8a87fa
yongseongjeon <yongseong.jeon95@gmail.com>
2022-10-10 17:41:38 +0900

    [SDR-353] feat: 로그인 페이지 문구 추가

--- fe/src/pages/Login/Login.tsx
+++ fe/src/pages/Login/Login.tsx
@@ -26,6 +26,7 @@ function Login() {
     <Wrap>
       <Form>
         <Logo />
+        <div>광고 리뷰는 싫어! 미식가들의 솔직 담백한 음식 리뷰 SNS! 🍱 식도락입니다.</div>
         <KakaoLogin href={`${process.env.REACT_APP_BE_SERVER_URL}/api/oauth/login`}>
           <LoginButton text={KAKAO} />
         </KakaoLogin>
