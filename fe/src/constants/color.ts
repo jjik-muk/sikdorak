7042600c11bcc065c906968ab312fd0344129ad2
yongseongjeon <yongseong.jeon95@gmail.com>
2022-10-18 16:27:12 +0900

    [SDR-384] refactor: color에 grey[100] 추가

--- fe/src/constants/color.ts
+++ fe/src/constants/color.ts
@@ -1,3 +1,13 @@
-export const COLOR = {
+export const COLOR: ColorType = {
   BLACK: '#000',
+  GREY: {
+    100: '#FEFEFE',
+  },
+};
+
+type ColorType = {
+  BLACK: '#000';
+  GREY: {
+    100: '#FEFEFE';
+  };
 };
