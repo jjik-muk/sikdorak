e6584735a8e8a8d6b26729fe0ce711a258dad84a
yongseongjeon <yongseong.jeon95@gmail.com>
2022-10-18 18:12:23 +0900

    [SDR-369] refactor: 스코프 관련 메세지 모듈화

--- fe/src/constants/text.ts
+++ fe/src/constants/text.ts
@@ -7,6 +7,10 @@ export const TEXT = {
     ID: '아이디',
     PASSWORD: '비밀번호',
   },
+  SCOPE: {
+    PUBLIC: '전체 공개합니다.',
+    PRIVATE: '친구에게만 공개합니다.',
+  },
   ALT: {
     PHOTOGRAPH: '유저가 업로드한 음식 또는 가게 사진',
   },
