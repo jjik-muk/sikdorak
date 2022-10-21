a4a1284c45c14125ee4447b1dbcce31b63e1a843
yongseongjeon <yongseong.jeon95@gmail.com>
2022-10-21 14:37:05 +0900

    [SDR-400] feat: 현 지도에서 재검색 버튼 추가 및 재검색

--- fe/src/pages/Map/Map.tsx
+++ fe/src/pages/Map/Map.tsx
@@ -1,4 +1,4 @@
-import { TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 import CommonHeader from 'components/Common/CommonHeader/CommonHeader';
@@ -12,6 +12,7 @@ import useSearchBar from 'hooks/useSearchBar';
 import useStores from 'hooks/useStores';
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { Z_INDEX } from 'styles/zIndex';
 import { createKey } from 'utils/utils';
 import { ContentArea, FeedsArea, MapArea } from './Map.styled';
 
@@ -20,7 +21,7 @@ const TABS = [{ label: '가게 목록' }, { label: '유저 리뷰' }];
 function Map() {
   const [activeTabIdx, setActiveTabIdx] = useState(0);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { stores, mapPos, setMapPos } = useStores();
+  const { stores, mapPos, setMapPos, fetchAndSetStores } = useStores();
   const { inputValue, searchResults, setInputValue, debouncedSearch } = useSearchBar();
   const { dispatchReviews } = useReviews();
   useAuth();
@@ -37,7 +38,7 @@ function Map() {
     debouncedSearch({
       url: `${process.env.REACT_APP_BE_SERVER_URL}/api/users?nickname=${inputValue}`,
     });
-  }, [debouncedSearch, inputValue]);
+  }, [inputValue]);
 
   return (
     <>
@@ -81,6 +82,9 @@ function Map() {
         </FeedsArea>
         <MapArea>
           <MapComponent stores={stores} mapPos={mapPos} setMapPos={setMapPos} />
+          <Button variant="contained" onClick={fetchAndSetStores} sx={{ position: 'absolute', top: '80%', left: '50%', zIndex: Z_INDEX.MAP_SEARCH_BTN }}>
+            현 지도에서 재검색
+          </Button>
         </MapArea>
       </ContentArea>
     </>
