69d50dd0f16aea05937e2d344c1089b270215856
SeokHo-Ham <kalejuice02@gmail.com>
2022-10-26 01:11:06 +0900

    SDR-408 BE/hot fix - ReviewDao의 isLast확인 로직도 수정

--- be/src/main/java/com/jjikmuk/sikdorak/review/command/domain/ReviewRepository.java
+++ be/src/main/java/com/jjikmuk/sikdorak/review/command/domain/ReviewRepository.java
@@ -3,6 +3,7 @@ package com.jjikmuk.sikdorak.review.command.domain;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,7 +17,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
         where r.userId = :userId and r.id <= :targetId
         order by r.id desc
         """)
-    List<Review> findByUserIdWithPageable(@Param("userId") long userId,
+    Slice<Review> findByUserIdWithPageable(@Param("userId") long userId,
         @Param("targetId") long targetId,
         Pageable pageable);
 
@@ -25,7 +26,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
         where r.userId = :userId and not r.reviewVisibility = 'PRIVATE' and r.id <= :targetId
         order by r.id desc
         """)
-    List<Review> findByUserIdAndConnection(@Param("userId") long userId,
+    Slice<Review> findByUserIdAndConnection(@Param("userId") long userId,
         @Param("targetId") long targetId,
         Pageable pageable);
 
@@ -34,7 +35,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
         where r.userId = :userId and r.reviewVisibility = 'PUBLIC' and r.id <= :targetId
         order by r.id desc
         """)
-    List<Review> findByUserIdAndDisconnection(@Param("userId") Long userId,
+    Slice<Review> findByUserIdAndDisconnection(@Param("userId") Long userId,
         @Param("targetId") long targetId,
         Pageable pageable);
 
@@ -46,7 +47,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
         order by (select count(*) from likes as l where l.review_id = r.review_id) desc, r.review_id desc 
         """,
         nativeQuery = true)
-    List<Review> findPublicRecommendedReviewsInRecentOrder(
+    Slice<Review> findPublicRecommendedReviewsInRecentOrder(
         @Param("targetId") long targetId,
         Pageable pageable);
 
@@ -56,7 +57,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
         and r.id <= :targetId and r.userId <> :authorId
         order by r.id desc
         """)
-    List<Review> findPublicAndProtectedRecommendedReviewsInRecentOrder(
+    Slice<Review> findPublicAndProtectedRecommendedReviewsInRecentOrder(
         @Param("authorId") long authorId,
         @Param("targetId") long targetId,
         Pageable pageable);
@@ -75,7 +76,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
            AND r.reviewVisibility = 'PUBLIC'
          ORDER BY r.id DESC
         """)
-    List<Review> findPublicReviewsOfStore(
+    Slice<Review> findPublicReviewsOfStore(
         @Param("storeId") long storeId,
         @Param("targetId") long targetId,
         Pageable pageable);
