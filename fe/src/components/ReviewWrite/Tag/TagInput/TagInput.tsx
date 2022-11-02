59ac90c5e1faa93d4057016cba945ad46abf64d2
yongseongjeon <yongseong.jeon95@gmail.com>
2022-11-02 17:24:44 +0900

    [SDR-417] feat: 태그에 한글, 영어, 숫자만 사용 가능하게 변경
    - 그 이외의 경우 경고창 표시

--- fe/src/components/ReviewWrite/Tag/TagInput/TagInput.tsx
+++ fe/src/components/ReviewWrite/Tag/TagInput/TagInput.tsx
@@ -1,6 +1,7 @@
 import TextField from '@mui/material/TextField';
 import { useReviewWrite } from 'context/ReviewWriteProvider';
 import { useState } from 'react';
+import { openWarningToast } from 'utils/toast';
 import { debounce } from 'utils/utils';
 
 function TagInput() {
@@ -22,10 +23,24 @@ function TagInput() {
   function handleTagAddition(e) {
     const isPressEnter = e.key === 'Enter';
     const hasInputValue = value.length > 0;
-    if (isPressEnter && hasInputValue) {
-      dispatchReviewWriteState({ type: 'ADD_TAG', tags: `${value}` });
-      setValue('');
+
+    if (!isPressEnter) return;
+    if (!hasInputValue) {
+      openWarningToast('태그를 입력해주세요.');
+      return;
     }
+    if (!validateTag(value)) {
+      openWarningToast('태그에 공백, 특수문자를 사용할 수 없습니다.');
+      return;
+    }
+    dispatchReviewWriteState({ type: 'ADD_TAG', tags: `${value}` });
+    setValue('');
+  }
+
+  function validateTag(tag: string) {
+    const allowedTagExp = /[^a-z|0-9|ㄱ-ㅎ|가-힣]/gi;
+    const res = tag.match(allowedTagExp);
+    return res === null;
   }
 }
 
