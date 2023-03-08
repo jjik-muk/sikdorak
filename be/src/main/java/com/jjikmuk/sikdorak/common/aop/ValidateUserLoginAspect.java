3f9a3f205847274fa1d6928c4894a0f794f12070
SeokHo-Ham <kalejuice02@gmail.com>
2023-03-08 18:22:14 +0900

    refactor: AdminOnly Aop 로직 수정

--- be/src/main/java/com/jjikmuk/sikdorak/common/aop/ValidateUserLoginAspect.java
+++ be/src/main/java/com/jjikmuk/sikdorak/common/aop/ValidateUserLoginAspect.java
@@ -2,15 +2,28 @@ package com.jjikmuk.sikdorak.common.aop;
 
 import com.jjikmuk.sikdorak.user.auth.api.LoginUser;
 import com.jjikmuk.sikdorak.user.auth.exception.NeedLoginException;
+import com.jjikmuk.sikdorak.user.user.command.app.UserService;
+import com.jjikmuk.sikdorak.user.user.command.domain.Authority;
+import com.jjikmuk.sikdorak.user.user.command.domain.User;
+import com.jjikmuk.sikdorak.user.user.command.domain.UserRepository;
+import com.jjikmuk.sikdorak.user.user.exception.NotFoundUserException;
+import com.jjikmuk.sikdorak.user.user.exception.UnauthorizedUserException;
 import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class ValidateUserLoginAspect {
+
+    private final UserRepository userRepository;
 
     @Before("@annotation(com.jjikmuk.sikdorak.common.aop.UserOnly) || @within(com.jjikmuk.sikdorak.common.aop.UserOnly)")
     public void validateUserOnly(JoinPoint joinPoint) {
@@ -21,7 +34,7 @@ public class ValidateUserLoginAspect {
     @Before("@annotation(com.jjikmuk.sikdorak.common.aop.AdminOnly) || @within(com.jjikmuk.sikdorak.common.aop.AdminOnly)")
     public void validateAdminOnly(JoinPoint joinPoint) {
         LoginUser loginUser = findLoginUser(joinPoint.getArgs());
-        loginUser.ifNotAdminThrowException();
+        validateAdmin(loginUser);
     }
 
     private LoginUser findLoginUser(Object... args) {
@@ -30,5 +43,16 @@ public class ValidateUserLoginAspect {
             .map(LoginUser.class::cast)
             .findFirst()
             .orElseThrow(NeedLoginException::new);
+    }
+
+    private void validateAdmin(LoginUser loginUser) {
+        loginUser.ifAnonymousThrowException();
+
+        User user = userRepository.findById(loginUser.getId())
+            .orElseThrow(NotFoundUserException::new);
+
+        if (!Authority.ADMIN.equals(user.getAuthority())) {
+            throw new UnauthorizedUserException();
+        }
     }
 }
