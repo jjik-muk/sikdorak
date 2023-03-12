757fbeb4a0223a393ec0e2c701b8b52eaf6e9477
SeokHo-Ham <kalejuice02@gmail.com>
2023-03-12 23:01:58 +0900

    refactor: 토큰의 payload에 authority 추가 및 decode메서드 반환 타입을 Map 타입으로 변경

--- be/src/main/java/com/jjikmuk/sikdorak/user/auth/app/AuthService.java
+++ be/src/main/java/com/jjikmuk/sikdorak/user/auth/app/AuthService.java
@@ -4,7 +4,11 @@ import com.jjikmuk.sikdorak.user.auth.api.LoginUser;
 import com.jjikmuk.sikdorak.user.auth.app.dto.JwtTokenPair;
 import com.jjikmuk.sikdorak.user.auth.exception.InvalidTokenException;
 import com.jjikmuk.sikdorak.user.user.command.app.UserService;
+import com.jjikmuk.sikdorak.user.user.command.domain.Authority;
+import com.jjikmuk.sikdorak.user.user.command.domain.User;
+import com.jjikmuk.sikdorak.user.user.command.domain.UserRepository;
 import com.jjikmuk.sikdorak.user.user.exception.NotFoundUserException;
+import java.util.Map;
 import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +21,7 @@ public class AuthService {
 
     private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String TOKEN_TYPE = "Bearer ";
+    private final UserRepository userRepository;
     private final UserService userService;
     private final JwtProvider jwtProvider;
 
@@ -24,13 +29,11 @@ public class AuthService {
     public JwtTokenPair reassignAccessToken(String refreshToken) {
 
         jwtProvider.validateToken(refreshToken);
-        String userId = jwtProvider.decodeToken(refreshToken);
+        Map<String, String> payloads = jwtProvider.decodeToken(refreshToken);
+        User user = userRepository.findById(Long.parseLong(payloads.get("id")))
+            .orElseThrow(NotFoundUserException::new);
 
-        if (!userService.isExistingById(Long.parseLong(userId))) {
-            throw new NotFoundUserException();
-        }
-
-        return jwtProvider.createTokenResponse(userId);
+        return jwtProvider.createTokenResponse(user);
     }
 
     public LoginUser authenticate(HttpServletRequest request) {
@@ -38,9 +41,15 @@ public class AuthService {
         try {
             String token = parseAuthorizationHeader(request);
             jwtProvider.validateToken(token);
-            Long userId = Long.valueOf(jwtProvider.decodeToken(token));
+            Map<String, String> payloads = jwtProvider.decodeToken(token);
 
-            return LoginUser.user(userId);
+            Long id = Long.parseLong(payloads.get("id"));
+            String authority = payloads.get("authority");
+
+            if (Authority.ADMIN.name().equals(authority)) {
+                return LoginUser.admin(id);
+            }
+            return LoginUser.user(id);
         }  catch (InvalidTokenException e) {
             return LoginUser.anonymous();
         }
--- be/src/main/java/com/jjikmuk/sikdorak/user/auth/app/JwtProvider.java
+++ be/src/main/java/com/jjikmuk/sikdorak/user/auth/app/JwtProvider.java
@@ -4,12 +4,17 @@ import com.jjikmuk.sikdorak.common.properties.JwtProperties;
 import com.jjikmuk.sikdorak.user.auth.app.dto.JwtTokenPair;
 import com.jjikmuk.sikdorak.user.auth.exception.ExpiredTokenException;
 import com.jjikmuk.sikdorak.user.auth.exception.InvalidTokenException;
+import com.jjikmuk.sikdorak.user.user.command.domain.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtBuilder;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import javax.crypto.SecretKey;
 import org.springframework.stereotype.Component;
 
@@ -24,26 +29,26 @@ public class JwtProvider {
         this.secretKey = Keys.hmacShaKeyFor(jwtProperties.getSecretKey());
     }
 
-    public JwtTokenPair createTokenResponse(String payload) {
+    public JwtTokenPair createTokenResponse(User user) {
 
         Date accessTokenExpiredDate = new Date(
             new Date().getTime() + jwtProperties.getAccessTokenExpiredMillisecond());
         Date refreshTokeExpiredDate = new Date(
             new Date().getTime() + jwtProperties.getRefreshTokenExpiredMillisecond());
+        Map<String, String> payloads = getPayloads(user);
 
-
-        String accessToken = createAccessToken(payload, accessTokenExpiredDate);
-        String refreshToken = createRefreshToken(payload, refreshTokeExpiredDate);
+        String accessToken = createAccessToken(payloads, accessTokenExpiredDate);
+        String refreshToken = createRefreshToken(payloads, refreshTokeExpiredDate);
 
         return new JwtTokenPair(accessToken, refreshToken);
     }
 
-    public String createAccessToken(String payload, Date accessTokenExpiredDate) {
-        return buildToken(payload, accessTokenExpiredDate);
+    public String createAccessToken(Map<String, String> payloads, Date accessTokenExpiredDate) {
+        return buildToken(payloads, accessTokenExpiredDate);
     }
 
-    public String createRefreshToken(String payload, Date refreshTokenExpiredDate) {
-        return buildToken(payload, refreshTokenExpiredDate);
+    public String createRefreshToken(Map<String, String> payloads, Date refreshTokenExpiredDate) {
+        return buildToken(payloads, refreshTokenExpiredDate);
     }
 
     public void validateToken(String token) {
@@ -59,21 +64,32 @@ public class JwtProvider {
         }
     }
 
-    public String decodeToken(String token) {
+    public Map<String, String> decodeToken(String token) {
         Claims claim = Jwts.parserBuilder()
             .setSigningKey(secretKey)
             .build()
             .parseClaimsJws(token)
             .getBody();
-
-        return claim.get("id", String.class);
+        return claim.keySet().stream()
+            .collect(Collectors.toMap(k -> k, k -> String.valueOf(claim.get(k))));
     }
 
-    private String buildToken(String payload, Date expiredDate) {
-        return Jwts.builder()
-            .claim("id", payload)
+    public Map<String, String> getPayloads(User user) {
+        Map<String, String> payloads = new HashMap<>();
+        payloads.put("id", String.valueOf(user.getId()));
+        payloads.put("authority", user.getAuthority().name());
+
+        return payloads;
+    }
+
+    private String buildToken(Map<String, String> payloads, Date expiredDate) {
+        JwtBuilder builder = Jwts.builder();
+        payloads.keySet().forEach(key -> builder.claim(key, payloads.get(key)));
+
+        return builder
             .setExpiration(expiredDate)
             .signWith(secretKey)
             .compact();
     }
+
 }
