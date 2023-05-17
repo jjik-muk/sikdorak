f3ccf944bb547062b50cbc78a1c552655d8fc4da
yongseongjeon <yongseong.jeon95@gmail.com>
2023-05-17 19:59:37 +0900

    [SDR-441] FE: FollowButton 테스트 생성
    - 팔로우 한/하지 않은 유저 프로필 방문시 팔로우/언팔로우 버튼 표시
    - 로그인 안된 상태로 팔로우 버튼 클릭하면 로그인 안내 문구 표시

---
+++ fe/src/components/UserDetail/FollowButton/FollowButton.test.js
@@ -0,0 +1,35 @@
+import { screen, waitFor } from '@testing-library/react';
+import FollowButton from './FollowButton';
+import { resetUserStore, setFollowStatus } from 'store/modules/user';
+import store from 'store/modules';
+import { ToastContainer } from 'react-toastify';
+import userEvent from '@testing-library/user-event';
+import { renderWithProviders } from 'utils/test';
+
+describe('팔로우 버튼', () => {
+  beforeEach(() => {
+    store.dispatch(resetUserStore());
+  });
+  it('팔로우 하지 않은 유저의 프로필을 방문한 경우 팔로우 버튼을 표시한다.', () => {
+    store.dispatch(setFollowStatus({ followStatus: false }));
+    renderWithProviders(<FollowButton />);
+    expect(screen.getByText('팔로우')).toBeInTheDocument();
+  });
+  it('팔로우한 유저의 프로필을 방문한 경우 언팔로우 버튼을 표시한다.', () => {
+    store.dispatch(setFollowStatus({ followStatus: true }));
+    renderWithProviders(<FollowButton />);
+    expect(screen.getByText('언팔로우'));
+  });
+  it('로그인이 안된 상태로 팔로우 버튼을 클릭하면 로그인 유도 문구를 표시한다.', async () => {
+    renderWithProviders(
+      <>
+        <FollowButton />
+        <ToastContainer />
+      </>,
+    );
+    userEvent.click(screen.getByText('팔로우'));
+    await waitFor(() => {
+      expect(screen.getByText('로그인이 필요한 서비스입니다. 로그인 해 주세요.')).toBeInTheDocument();
+    });
+  });
+});
